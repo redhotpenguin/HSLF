d2176f7f682357c16ac1790214e6fbe6f7fe1ea9
jonas <jonas@portlandia.(none)>
2012-03-08 15:32:33 -0800

    Removed Gii

--- html/protected/config/main.php
+++ html/protected/config/main.php
@@ -19,15 +19,7 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'giipass',
-		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('*','::1'),
-		),
-		
+	
 	),
 
 	// application components
