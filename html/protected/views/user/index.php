97326045746cd81411e5e9131ce5be4da670f518
Jonas Palmero <jonas@winningmark.com>
2012-03-13 13:58:50 -0700

    new view

---
+++ html/protected/views/user/index.php
@@ -0,0 +1,26 @@
+<?php
+$this->breadcrumbs=array(
+	'Users',
+);
+
+$this->menu=array(
+	array('label'=>'Create User', 'url'=>array('create')),
+	array('label'=>'Manage User', 'url'=>array('admin')),
+);
+?>
+
+<h1>Users</h1>
+
+<?php 
+
+$this->widget('zii.widgets.grid.CGridView', array(
+    'dataProvider'=>$dataProvider,
+    'columns'=>array(        
+        'username',  
+        'email',
+        array(            // display a column with "view", "update" and "delete" buttons
+            'class'=>'CButtonColumn',
+        ),
+    ),
+));
+?>
