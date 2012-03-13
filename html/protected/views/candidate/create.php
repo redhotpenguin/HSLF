98464a1ff09baed7b05bb57480b780afa8be526f
Jonas Palmero <jonas@winningmark.com>
2012-03-13 13:58:42 -0700

    new view

---
+++ html/protected/views/candidate/create.php
@@ -0,0 +1,15 @@
+<?php
+$this->breadcrumbs=array(
+	'Candidates'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List Candidate', 'url'=>array('index')),
+	array('label'=>'Manage Candidate', 'url'=>array('admin')),
+);
+?>
+
+<h1>Create Candidate</h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
