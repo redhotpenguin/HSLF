3e00f78800671ee200067dcd13841c10861ecd8b
Jonas Palmero <jonas@winningmark.com>
2012-03-13 13:56:01 -0700

    model update

---
+++ html/protected/models/PushNotifications.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This is the model class for table "push_notifications".
+ *
+ * The followings are the available columns in table 'push_notifications':
+ * @property integer $id
+ * @property string $message
+ * @property string $sent
+ * @property string $create_time
+ */
+class PushNotifications extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return PushNotifications the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'push_notifications';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('message', 'required'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, message, sent, create_time', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'message' => 'Message',
+			'sent' => 'Sent',
+			'create_time' => 'Create Time',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('message',$this->message,true);
+		$criteria->compare('sent',$this->sent,true);
+		$criteria->compare('create_time',$this->create_time,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
