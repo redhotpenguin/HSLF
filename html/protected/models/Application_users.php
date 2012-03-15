d7fcbf7d8924000981634404d4abdd12f7c86a54
Jonas Palmero <jonas@winningmark.com>
2012-03-15 12:57:31 -0700

    updated application_users model

--- html/protected/models/Application_users.php
+++ html/protected/models/Application_users.php
@@ -18,114 +18,109 @@
  * @property State $stateAbbr
  * @property District $district0
  */
-class Application_users extends CActiveRecord
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Application_users the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class Application_users extends CActiveRecord {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'app_users';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return Application_users the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-                        array('device_token, state_abbr, district, type','required'),
-			array('district', 'numerical', 'integerOnly'=>true),
-			array('device_token', 'length', 'max'=>128),
-			array('state_abbr', 'length', 'max'=>3),
-			array('user_agent', 'length', 'max'=>1024),
-			array('latitude, longitude, registration, type', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, device_token, latitude, longitude, state_abbr, district, registration, type, user_agent', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'app_users';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'stateAbbr' => array(self::BELONGS_TO, 'State', 'state_abbr'),
-			'district0' => array(self::BELONGS_TO, 'District', 'district'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('device_token, state_abbr, district, type', 'required'),
+            array('district', 'numerical', 'integerOnly' => true),
+            array('device_token', 'length', 'max' => 128),
+            array('state_abbr', 'length', 'max' => 3),
+            array('user_agent', 'length', 'max' => 1024),
+            array('latitude, longitude, registration, type', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, device_token, latitude, longitude, state_abbr, district, registration, type, user_agent', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'device_token' => 'Device Token',
-			'latitude' => 'Latitude',
-			'longitude' => 'Longitude',
-			'state_abbr' => 'State Abbr',
-			'district' => 'District',
-			'registration' => 'Registration',
-			'type' => 'Type',
-			'user_agent' => 'User Agent',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'stateAbbr' => array(self::BELONGS_TO, 'State', 'state_abbr'),
+            'district0' => array(self::BELONGS_TO, 'District', 'district'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'device_token' => 'Device Token',
+            'latitude' => 'Latitude',
+            'longitude' => 'Longitude',
+            'state_abbr' => 'State Abbr',
+            'district' => 'District',
+            'registration' => 'Registration',
+            'type' => 'Type',
+            'user_agent' => 'User Agent',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('device_token',$this->device_token,true);
-		$criteria->compare('latitude',$this->latitude,true);
-		$criteria->compare('longitude',$this->longitude,true);
-		$criteria->compare('state_abbr',$this->state_abbr,true);
-		$criteria->compare('district',$this->district);
-		$criteria->compare('registration',$this->registration,true);
-		$criteria->compare('type',$this->type,true);
-		$criteria->compare('user_agent',$this->user_agent,true);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-        
-        public function beforeSave(){
-              if ($this->isNewRecord){
-                     $this->registration = new CDbExpression('NOW()');
-                     
-                     if(isset($_SERVER['HTTP_USER_AGENT']))
-                         $this->user_agent = $_SERVER['HTTP_USER_AGENT']; //should really be in the controller\
-                     else
-                          $this->user_agent = 'UNAVALAIBLE';
-              }
+        $criteria->compare('id', $this->id);
+        $criteria->compare('device_token', $this->device_token, true);
+        $criteria->compare('latitude', $this->latitude, true);
+        $criteria->compare('longitude', $this->longitude, true);
+        $criteria->compare('state_abbr', $this->state_abbr, true);
+        $criteria->compare('district', $this->district);
+        $criteria->compare('registration', $this->registration, true);
+        $criteria->compare('type', $this->type, true);
+        $criteria->compare('user_agent', $this->user_agent, true);
 
- 
-                return parent::beforeSave();
+        return new CActiveDataProvider($this, array(
+                    'criteria' => $criteria,
+                ));
+    }
+
+    public function beforeSave() {
+        if ($this->isNewRecord) {
+            $this->registration = new CDbExpression('NOW()');
+
+            if (isset($_SERVER['HTTP_USER_AGENT']))
+                $this->user_agent = $_SERVER['HTTP_USER_AGENT']; //should really be in the controller\
+            else
+                $this->user_agent = 'UNAVALAIBLE';
         }
+
+
+        return parent::beforeSave();
+    }
+
 }
