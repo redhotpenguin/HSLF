8c471e7197f301e2399b99b571c9324efa6d8a10
Jonas Palmero <jonas@winningmark.com>
2012-03-15 13:50:22 -0700

    html/protected/controllers/PushNotificationsController.php

--- html/protected/tests/bootstrap.php
+++ html/protected/tests/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 
 // change the following paths if necessary
+ session_start();
+ ob_start();
 $yiit=dirname(__FILE__).'/../../../../framework/yiit.php';
+
+$yiit = 'C:/xampp/htdocs/HSLF/framework/yiit.php';
+
+
 $config=dirname(__FILE__).'/../config/test.php';
+
+$config = 'C:/xampp/htdocs/HSLF/html/protected/config/test.php';
 
 require_once($yiit);
 require_once(dirname(__FILE__).'/WebTestCase.php');
