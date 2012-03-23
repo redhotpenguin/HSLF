b145c337921fd8f3a7c46f3974c1614d9b4dd614
Jonas Palmero <jonas@winningmark.com>
2012-03-23 16:48:27 -0700

    updates

--- html/protected/views/site/index.php
+++ html/protected/views/site/index.php
@@ -5,23 +5,37 @@
 
 <div style="height:600px;">
 
-    <div id="last_candidate">
+    <div class="last_entry candidate">
+        <h3>Last Candidate:</h3>
+         <h6><?php echo $last_candidate->full_name; ?></h6>
         <?php
-        echo $last_candidate->full_name;
+        
+        echo $last_candidate->endorsement;
+        echo  '<br/><br/>Published: '.$last_candidate->date_published;
+        echo  '<br/>Scorecard: '.$last_candidate->scorecard;
+        echo  '<br/>State: '.$last_candidate->state_abbr;
+        echo '<br/>Party '.$last_candidate->party;
         ?>
     </div>
 
-    <div id="last_push">
+    <div class="last_entry push">
+        <h3>Last Notification:</h3>
         <?php
         echo $last_push->message;
+        echo '<br/><br/>Published: '.$last_push->create_time;
         ?>
     </div>
 
 
-    <div id="last_alert">
-        <?php
-        echo $last_alert->content;
-        ?>
+    <div class="last_entry alert">
+        <h3>Last Alert:</h3>
+        <h6><?php   echo $last_alert->title; ?></h6>
+        <div><?php echo $last_alert->content; ?> </div>
+        <div>Published <?php echo $last_alert->create_time; ?> </div>
+        <div>State: <?php echo $last_alert->state_abbr; ?> </div>
+        
+        
+    
     </div>
 
 
