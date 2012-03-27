9fe2bab8d9783995b90d24a3067ea4baf9749f70
Jonas Palmero <jonas@winningmark.com>
2012-03-27 12:14:27 -0700

    district tree view

---
+++ html/js/form/push.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($){
+  
+    var audience_selector = $("input[name='audience_type']");
+    var audience_target = $("#audience_target");
+    var confirm_broadcast = $('<label>Confirm broadcast:</label><input type="checkbox" name="confirm_broadcast">');
+
+
+
+
+    audience_selector.change(function(){
+        switch(this.value){
+            case 'district':
+                audience_target.addDistrictTree();
+                break;
+       
+            case 'broadcast':
+                audience_target.addConfirmationForm();
+                break;
+        }
+    });
+
+    jQuery.fn.addConfirmationForm = function() {
+        var o = $(this[0]);
+        o.html( confirm_broadcast );
+        return o;
+    }
+
+
+    jQuery.fn.addDistrictTree = function() {
+        var o = $(this[0]);
+    
+        $.ajax({
+            url: "/district/getTreeView",
+            success: function(data) {
+                o.html(data);
+            }
+        });
+        return o;
+    }
+
+    //simulate event when page is first loaded
+   // $('input[name=audience_type]:checked').change(); 
+});
