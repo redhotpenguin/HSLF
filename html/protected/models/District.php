58a23a1ae94882083d805f8697228dcc663a5189
Jonas Palmero <jonas@winningmark.com>
2012-04-02 14:15:43 -0700

    fixed ordering on list and admin(manage) views

--- html/protected/models/District.php
+++ html/protected/models/District.php
@@ -11,99 +11,92 @@
  * The followings are the available model relations:
  * @property State $stateAbbr
  */
-class District extends CActiveRecord
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return District the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class District extends CActiveRecord {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'district';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return District the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('state_abbr, number', 'required'),
-			array('number', 'numerical', 'integerOnly'=>true),
-			array('state_abbr', 'length', 'max'=>3),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, state_abbr, number', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'district';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'candidates' => array(self::HAS_MANY, 'Candidate', 'district_id'),
-                        'user_alerts' => array(self::HAS_MANY, 'User_alert', 'district_id'),
-			'stateAbbr' => array(self::BELONGS_TO, 'State', 'state_abbr'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('state_abbr, number', 'required'),
+            array('number', 'numerical', 'integerOnly' => true),
+            array('state_abbr', 'length', 'max' => 3),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, state_abbr, number', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'state_abbr' => 'State Abbreviation',
-			'number' => 'District Number',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'candidates' => array(self::HAS_MANY, 'Candidate', 'district_id'),
+            'user_alerts' => array(self::HAS_MANY, 'User_alert', 'district_id'),
+            'stateAbbr' => array(self::BELONGS_TO, 'State', 'state_abbr'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'state_abbr' => 'State Abbreviation',
+            'number' => 'District Number',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+   
+        $criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('state_abbr',$this->state_abbr,true);
-		$criteria->compare('number',$this->number);
+        $criteria->compare('id', $this->id);
+        $criteria->compare('state_abbr', $this->state_abbr, true);
+        $criteria->compare('number', $this->number);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-        
-        /**
-	 * Retrieve the District ID based on $state and $district_number
-	 */
-     
-      
-         public static function getIdByStateAndDistrict($state, $district_number){
-            $district =  District::model()->findByAttributes( array ( 'state_abbr' => $state,'number' => $district_number ) );
-            if($district)
-                return $district->id;
-            else 
-                return false;
-         }
+        return new CActiveDataProvider($this, array(
+                    'criteria' => $criteria,
+                    'sort' => array(
+                        'defaultOrder' => 'state_abbr ASC')
+                ));
+    }
+
+    /**
+     * Retrieve the District ID based on $state and $district_number
+     */
+    public static function getIdByStateAndDistrict($state, $district_number) {
+        $district = District::model()->findByAttributes(array('state_abbr' => $state, 'number' => $district_number));
+        if ($district)
+            return $district->id;
+        else
+            return false;
+    }
+
 }
