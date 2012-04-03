5b3b454cc8e29a5cbe43b295e8f7c42618f2a083
Jonas Palmero <jonas@winningmark.com>
2012-04-03 15:19:09 -0700

    district tree view: ordered state

--- html/protected/extensions/DistrictTreeView/DistrictTreeView.php
+++ html/protected/extensions/DistrictTreeView/DistrictTreeView.php
@@ -11,13 +11,6 @@ class DistrictTreeView extends CJuiWidget {
 
         if ($this->baseUrl === '')
             throw new CException(Yii::t('JSTree', 'baseUrl must be set.'));
-        /*
-          $clientScript = Yii::app()->getClientScript();
-          $clientScript->registerScriptFile($this->baseUrl.'/jstree/jquery.jstree.js');
-          $clientScript->registerScriptFile($this->baseUrl.'/jstree/_lib/jquery.cookie.js');
-          $clientScript->registerScriptFile($this->baseUrl.'/jstree/_lib/jquery.hotkeys.js');
-          $clientScript->registerCssFile($this->baseUrl.'/jstree/themes/apple/style.css');
-         */
     }
 
     public function publishAssets() {
@@ -41,50 +34,26 @@ class DistrictTreeView extends CJuiWidget {
         $this->registerClientScripts();
         $ul_id = $this->options['id'];
 
-        $states = State::model()->findAll();
+        $states = State::model()->findAll(array('order' => 'abbr ASC') );
         $finalList = '';
 
 
         $state_district = array();
-        foreach ($states as $state) {
-
-            $finalList.='<div class="state_group">';
-
-            $params = array(
-                'order' => 'number ASC',
-            );
-
-            $districts = District::model()->findAllByAttributes(array('state_abbr' => $state->abbr), $params);
-
-            $finalList.= "<label>  $state->name  </label> ";
-
-            foreach ($districts as $district) {
-                $finalList.= '<input type="checkbox" name="district_ids[]" value="' . $district->id . '"> ' . $district->number . ' <br/>';
-            }
-
-            $finalList.= '</div>';
-        }
-        ?>
-
-
-
-        <?php
+   
+       
 
     }
 
     private function getData() {
 
         $i = 0;
-        $states = State::model()->findAll();
+        $states = State::model()->findAll(array('order' => 'abbr ASC'));
         $data = array();
-        $params = array('order' => 'number ASC',);
+        $params = array('order' => 'number ASC');
 
         $state_district = array();
        
         foreach ($states as $state) {
-            if($state->abbr == 'na'){
-                continue;
-            }
              $row = array();
              $row['id'] = $state->abbr;
              $row['text'] = $state->name;
@@ -110,23 +79,7 @@ class DistrictTreeView extends CJuiWidget {
 
                array_push($data, $row);
         }
-        
- 
 
-
-        //  return $data;
-
-
-
-       /* return $data = array(
-            array('id' => 'al',
-                'text' => 'Alabama',
-                "children" => array(
-                    array('id' => 1, 'text' => '<input type="checkbox" name="district_ids[]" value="1" ><label>1</label>'),
-                    array('id' => 2, 'text' => 2),
-                ),
-            ),
-        );*/
         
         return $data;
     }
