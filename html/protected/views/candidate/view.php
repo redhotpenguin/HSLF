9f06eb481acd30a02e2759cf1d6d019a4b18b298
Jonas Palmero <jonas@winningmark.com>
2012-04-03 12:43:20 -0700

    candidate view updates

--- html/protected/views/candidate/view.php
+++ html/protected/views/candidate/view.php
@@ -1,40 +1,21 @@
 <?php
-$this->breadcrumbs=array(
-	'Candidates'=>array('index'),
-	$model->id,
+$this->breadcrumbs = array(
+    'Candidates' => array('index'),
+    $model->id,
 );
 
-$this->menu=array(
-	array('label'=>'List Candidate', 'url'=>array('index')),
-	array('label'=>'Create Candidate', 'url'=>array('create')),
-	array('label'=>'Update Candidate', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Candidate', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Candidate', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'List Candidate', 'url' => array('index')),
+    array('label' => 'Create Candidate', 'url' => array('create')),
+    array('label' => 'Update Candidate', 'url' => array('update', 'id' => $model->id)),
+    array('label' => 'Delete Candidate', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+    array('label' => 'Manage Candidate', 'url' => array('admin')),
 );
 ?>
 
 <h1><?php echo $model->full_name; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'state_abbr',
-		array(
-                    'name' => 'district_id',
-                    'value'=>$model->district->number,
-                ),
-		'type',
-		'endorsement',
-		'full_name',
-		'party',
-		'date_published',
-		'publish',
-                'scorecard',
-	),
-)); ?>
-
-<div id="candidate_preview" style="margin-top:15px;">
+<div id="candidate_preview">
     <div id="iphone_bg">
         <div id="Candidate_full_name_preview" class="input_preview"><?php echo $model->full_name; ?></div>
         <div id="Candidate_state_abbr_preview" class="list_preview"><?php echo $model->state_abbr; ?></div>
@@ -46,3 +27,29 @@ $this->menu=array(
         <div id="Candidate_endorsement_preview" class="input_preview"><?php echo $model->endorsement; ?></div>
     </div>
 </div>
+
+
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+        'id',
+        'state_abbr',
+        array(
+            'name' => 'district_id',
+            'value' => $model->district->number,
+        ),
+        'type',
+        array(
+            'header' => 'endorsement',
+            'value' => substr(strip_tags($model->endorsement), 0, 300) . '...',
+        ),
+        'full_name',
+        'party',
+        'date_published',
+        'publish',
+        'scorecard',
+    ),
+));
+?>
+
