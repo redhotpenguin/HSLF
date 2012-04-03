18486a57d29958f0cb897481eba507e17a46ebe0
Jonas Palmero <jonas@winningmark.com>
2012-04-03 12:25:23 -0700

    candidate view updates

--- html/themes/hslf/views/layouts/main.php
+++ html/themes/hslf/views/layouts/main.php
@@ -36,83 +36,89 @@
     <body>
 
         <div class="container" id="page">
+            <div class="<?php echo $this->getId(); ?>">
+                <div class="<?php echo $this->getAction()->getId(); ?>" >
 
-            <div id="header">
-                <div id="logo"><a href="/"><?php echo CHtml::encode(Yii::app()->name); ?></a></div>
 
-                <div id="menu_auth">
-<?php
-$this->widget('zii.widgets.CMenu', array(
-    'items' => array(
-        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
-        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
-    ),
-));
-?>
-                </div>
+                    <div id="header">
+                        <div id="logo"><a href="/"><?php echo CHtml::encode(Yii::app()->name); ?></a></div>
 
-            </div><!-- header -->
-<?php
-if (Yii::app()->user->id):
-    ?>
+                        <div id="menu_auth">
+                            <?php
+                            $this->widget('zii.widgets.CMenu', array(
+                                'items' => array(
+                                    array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                                    array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
+                                ),
+                            ));
+                            ?>
+                        </div>
 
-                <div id="menu-top" class="clearfix">
-                <?php
-                $this->widget('zii.widgets.CMenu', array(
-                    'activeCssClass' => 'active',
-                    'activateParents' => true,
-                    'items' => array(
-                        array(
-                            'label' => 'Publishing',
-                            'itemOptions' => array('id' => 'itemPublishing'),
-                            'url' => array('/site/publishing'),
-                            'visible' => !Yii::app()->user->isGuest,
-                            'items' => array(
-                                array('label' => 'State', 'url' => array('/state'), 'visible' => !Yii::app()->user->isGuest),
-                                array('label' => 'District', 'url' => array('/district'), 'visible' => !Yii::app()->user->isGuest),
-                                array('label' => 'Candidate', 'url' => array('/candidate'), 'visible' => !Yii::app()->user->isGuest),
-                            ),
-                        ),
-                        array(
-                            'label' => 'Messaging Center',
-                            'url' => array('/site/messaging'),
-                            'visible' => !Yii::app()->user->isGuest,
-                            'itemOptions' => array('id' => 'itemMessaging'),
-                            'items' => array(
-                                array('label' => 'User Alerts', 'url' => array('/user_alert'), 'visible' => !Yii::app()->user->isGuest),
-                                array('label' => 'Push Notifications', 'url' => array('/pushNotifications'), 'visible' => !Yii::app()->user->isGuest),
-                            ),
-                        ),
-                        array(
-                            'label' => 'Administration',
-                            'url' => array('/site/administration'),
-                            'visible' => !Yii::app()->user->isGuest,
-                            'itemOptions' => array('id' => 'itemAdministration'),
-                            'items' => array(
-                                array('label' => 'Application Users', 'url' => array('/application_users'), 'visible' => !Yii::app()->user->isGuest),
-                                array('label' => 'Users', 'url' => array('/user'), 'visible' => !Yii::app()->user->isGuest),
-                            ),
-                        ),
-                    ),
-                ));
-                ?>
-                </div>
+                    </div><!-- header -->
                     <?php
-                endif;
-                ?>
+                    if (Yii::app()->user->id):
+                        ?>
+
+                        <div id="menu-top" class="clearfix">
+                            <?php
+                            $this->widget('zii.widgets.CMenu', array(
+                                'activeCssClass' => 'active',
+                                'activateParents' => true,
+                                'items' => array(
+                                    array(
+                                        'label' => 'Publishing',
+                                        'itemOptions' => array('id' => 'itemPublishing'),
+                                        'url' => array('/site/publishing'),
+                                        'visible' => !Yii::app()->user->isGuest,
+                                        'items' => array(
+                                            array('label' => 'State', 'url' => array('/state'), 'visible' => !Yii::app()->user->isGuest),
+                                            array('label' => 'District', 'url' => array('/district'), 'visible' => !Yii::app()->user->isGuest),
+                                            array('label' => 'Candidate', 'url' => array('/candidate'), 'visible' => !Yii::app()->user->isGuest),
+                                        ),
+                                    ),
+                                    array(
+                                        'label' => 'Messaging Center',
+                                        'url' => array('/site/messaging'),
+                                        'visible' => !Yii::app()->user->isGuest,
+                                        'itemOptions' => array('id' => 'itemMessaging'),
+                                        'items' => array(
+                                            array('label' => 'User Alerts', 'url' => array('/user_alert'), 'visible' => !Yii::app()->user->isGuest),
+                                            array('label' => 'Push Notifications', 'url' => array('/pushNotifications'), 'visible' => !Yii::app()->user->isGuest),
+                                        ),
+                                    ),
+                                    array(
+                                        'label' => 'Administration',
+                                        'url' => array('/site/administration'),
+                                        'visible' => !Yii::app()->user->isGuest,
+                                        'itemOptions' => array('id' => 'itemAdministration'),
+                                        'items' => array(
+                                            array('label' => 'Application Users', 'url' => array('/application_users'), 'visible' => !Yii::app()->user->isGuest),
+                                            array('label' => 'Users', 'url' => array('/user'), 'visible' => !Yii::app()->user->isGuest),
+                                        ),
+                                    ),
+                                ),
+                            ));
+                            ?>
+                        </div>
+                        <?php
+                    endif;
+                    ?>
 
 
-            <?php echo $content; ?>
+                    <?php echo $content; ?>
 
-            <div class="clear"></div>
+                    <div class="clear"></div>
 
-            <div id="footer">
-                Copyright &copy; <?php echo date('Y'); ?> by Winning Mark.<br/>
-                All Rights Reserved.<br/>
 
-            </div><!-- footer -->
+                    <div id="footer">
+                        Copyright &copy; <?php echo date('Y'); ?> by Winning Mark.<br/>
+                        All Rights Reserved.<br/>
 
+                    </div><!-- footer -->
+                </div>
+            </div>
         </div><!-- page -->
+
 
     </body>
 </html>
