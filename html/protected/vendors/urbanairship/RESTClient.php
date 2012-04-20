1b495ea2d9420e419bf0402c6ab4f3c982b09bb8
Jonas <jonas@winningmark.com>
2012-04-20 08:54:59 -0700

    updated restclient to use http_request2

--- html/protected/vendors/urbanairship/RESTClient.php
+++ html/protected/vendors/urbanairship/RESTClient.php
@@ -1,9 +1,10 @@
 <?php
+
 /*
- Thanks George A. Papayiannis, most codes of this file are from
- http://www.sematopia.com/2006/10/how-to-making-a-php-rest-client-to-call-rest-resources/
+  Thanks George A. Papayiannis, most codes of this file are from
+  http://www.sematopia.com/2006/10/how-to-making-a-php-rest-client-to-call-rest-resources/
  */
-require_once "HTTP/Request.php";
+require_once "HTTP/Request2.php";
 
 class RESTClient {
 
@@ -16,7 +17,7 @@ class RESTClient {
     private $responseCode = "";
     private $req = null;
 
-    public function __construct($user_name="", $password="", $content_type="") {
+    public function __construct($user_name = "", $password = "", $content_type = "") {
         $this->user_name = $user_name;
         $this->password = $password;
         $this->content_type = $content_type;
@@ -25,28 +26,31 @@ class RESTClient {
 
     public function createRequest($url, $method, $arr = null) {
         $this->curr_url = $url;
-        $this->req = new HTTP_Request($url);
+        $this->req = new HTTP_Request2($url);
+        $this->req->setConfig('ssl_verify_peer', false);
+
         if ($this->user_name != "" && $this->password != "") {
-           $this->req->setBasicAuth($this->user_name, $this->password);
+            $this->req->setAuth($this->user_name, $this->password);
         }
         if ($this->content_type != "") {
-           $this->req->addHeader("Content-Type", $this->content_type);
+            $this->req->setHeader("Content-Type", $this->content_type);
         }
 
-        switch($method) {
+        switch ($method) {
             case "GET":
-                $this->req->setMethod(HTTP_REQUEST_METHOD_GET);
+                $this->req->setMethod('GET');
                 break;
             case "POST":
-                $this->req->setMethod(HTTP_REQUEST_METHOD_POST);
+                $this->req->setMethod('POST');
+
                 $this->addPostData($arr);
                 break;
             case "PUT":
-                $this->req->setMethod(HTTP_REQUEST_METHOD_PUT);
+                $this->req->setMethod('PUT');
                 $this->addPostData($arr);
                 break;
             case "DELETE":
-                $this->req->setMethod(HTTP_REQUEST_METHOD_DELETE);
+                $this->req->setMethod('DELETE');
                 break;
         }
     }
@@ -64,15 +68,16 @@ class RESTClient {
     }
 
     public function sendRequest() {
-        $this->response = $this->req->sendRequest();
-
-        if (PEAR::isError($this->response)) {
-            echo $this->response->getMessage();
-            die();
-        } else {
-            $this->responseCode = $this->req->getResponseCode();
-            $this->responseBody = $this->req->getResponseBody();
+        try {
+            $this->response = $this->req->send();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            error_log("RestClient, sendRequest error:" . $e->getMessage());
         }
+
+
+        $this->responseCode = $this->response->getStatus();
+        $this->responseBody = $this->response->getBody();
     }
 
     public function getResponse() {
