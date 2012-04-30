b203d5553d83f4238707e621741310510ffefedf
Jonas <jonas@winningmark.com>
2012-04-30 11:32:59 -0700

    renamed back-end name

--- html/protected/config/main.php
+++ html/protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'MVG Application - HSLF',
+    'name' => 'Winning Mark Mobile',
     // preloading 'log' component
     'preload' => array('log'),
     // autoloading model and component classes
