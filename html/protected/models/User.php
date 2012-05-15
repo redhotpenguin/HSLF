09c435c584f6656973d9aa512841ffcf740246df
Jonas <jonas@winningmark.com>
2012-05-15 09:26:32 -0700

    user management update

--- html/protected/models/User.php
+++ html/protected/models/User.php
@@ -35,8 +35,6 @@ class User extends CActiveRecord {
      * @return array validation rules for model attributes.
      */
     public function rules() {
-        // NOTE: you should only define rules for those attributes that
-        // will receive user inputs.
         return array(
             array('username, password, email, role', 'required'),
             array('email', 'email'),
@@ -52,8 +50,6 @@ class User extends CActiveRecord {
      * @return array relational rules.
      */
     public function relations() {
-        // NOTE: you may need to adjust the relation name and the related
-        // class name for the relations automatically generated below.
         return array(
         );
     }
