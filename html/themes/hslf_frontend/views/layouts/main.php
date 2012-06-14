4cbd9ae2cb71701aed06963571ac8a6516ae6014
Jonas <jonas@winningmark.com>
2012-06-14 15:20:16 -0700

    clean front end

--- html/themes/hslf_frontend/views/layouts/main.php
+++ html/themes/hslf_frontend/views/layouts/main.php
@@ -1,1 +1,14 @@
-HTML goes here!
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+        <link rel="stylesheet" type="text/css" href="/themes/hslf_frontend/css/main.css" />
+
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+        <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+    </head>
+
+    <body>
+        HTML goes here!
+    </body>
+</html>
