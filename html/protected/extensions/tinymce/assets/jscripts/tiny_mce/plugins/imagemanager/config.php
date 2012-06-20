b76bdbad716e4db32ad75569c7b4c7b2d2135564
Jonas <jonas@winningmark.com>
2012-06-20 11:25:00 -0700

    image manager config

--- html/protected/extensions/tinymce/assets/jscripts/tiny_mce/plugins/imagemanager/config.php
+++ html/protected/extensions/tinymce/assets/jscripts/tiny_mce/plugins/imagemanager/config.php
@@ -33,7 +33,7 @@
 	// General filesystem options
 	$mcImageManagerConfig['filesystem'] = "Moxiecode_LocalFileImpl";
 	$mcImageManagerConfig['filesystem.path'] = ''; // absolute or relative from this script path, optional.
-	$mcImageManagerConfig['filesystem.rootpath'] = '/var/www/mvg.winningmark.com/html/img'; // absolute or relative from this script path, required.
+	$mcImageManagerConfig['filesystem.rootpath'] = '/var/www/mvg.winningmark.com/html/content/img'; // absolute or relative from this script path, required.
 	$mcImageManagerConfig['filesystem.datefmt'] = "Y-m-d H:i";
 	$mcImageManagerConfig['filesystem.include_directory_pattern'] = '';
 	$mcImageManagerConfig['filesystem.exclude_directory_pattern'] = '/^mcith$/i';
