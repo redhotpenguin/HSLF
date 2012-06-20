a3a483d4ad50e52517c302c86b41a52ebc962520
Fred Moyer <fred@redhotpenguin.com>
2012-06-20 10:19:48 -0700

    Remove conflicts

--- html/protected/extensions/tinymce/TinyMce.php
+++ html/protected/extensions/tinymce/TinyMce.php
@@ -47,11 +47,7 @@ class TinyMce extends CInputWidget {
                     mode:'exact',
                     // General options
                     theme : "advanced",
-<<<<<<< HEAD
-                    plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist,imagemanager",
-=======
                     plugins : "imagemanager,autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist",
->>>>>>> 6780a824bab8b7e058cfbf1d5065b62440cc8c4a
                     theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
                     theme_advanced_buttons2 : "insertimage,cut,copy,paste,pastetext,pasteword,|,search,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
                     theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
