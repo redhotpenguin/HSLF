f78abe0313c8b37ca03a4eb79b83a428185976b3
Fred Moyer <fred@redhotpenguin.com>
2012-06-20 14:22:32 -0700

    Tab and page for image manager.

---
+++ html/protected/admin/controllers/UploadController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class UploadController extends Controller {
+
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
+    public $category = array('Publishing' => array('/site/publishing/')); // used by the breadcrumb
+
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+
+        return array(
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('index'),
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+
+    public function actionIndex() {
+        $this->render('index', array(
+        ));
+    }
+
+}
+
+?>
