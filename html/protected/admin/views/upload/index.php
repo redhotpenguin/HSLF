829eb7afabf8475d17eabfee2f3a773756bd333b
Fred Moyer <fred@redhotpenguin.com>
2012-06-20 15:05:29 -0700

    Add view

---
+++ html/protected/admin/views/upload/index.php
@@ -0,0 +1,14 @@
+<?php
+$this->breadcrumbs = array(
+    'Upload Images',
+);
+/*
+$this->menu = array(
+    array('label' => 'Upload an image', 'url' => array('create')),
+);
+*/
+?>
+
+<h1>Image Upload</h1>
+
+<iframe src="/imagemanager/pages/im/index.html" width="800" height="400" style="border:1px solid"></iframe>
