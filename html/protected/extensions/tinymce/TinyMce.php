742b6fde2a9ffde6a1c2eef74ac5cd149e3fdcfa
Fred Moyer <fred@redhotpenguin.com>
2012-06-20 10:11:03 -0700

    Merge from dev

--- html/protected/extensions/tinymce/TinyMce.php
+++ html/protected/extensions/tinymce/TinyMce.php
@@ -47,9 +47,13 @@ class TinyMce extends CInputWidget {
                     mode:'exact',
                     // General options
                     theme : "advanced",
+<<<<<<< HEAD
+                    plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist,imagemanager",
+=======
                     plugins : "imagemanager,autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,iespell,inlinepopups,media,searchreplace,print,contextmenu,paste,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,advlist",
+>>>>>>> 6780a824bab8b7e058cfbf1d5065b62440cc8c4a
                     theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-                    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                    theme_advanced_buttons2 : "insertimage,cut,copy,paste,pastetext,pasteword,|,search,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
                     theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
                     theme_advanced_buttons4 : "moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking",
                     theme_advanced_toolbar_location : "top",
