54079a3e7565385d57c8c483e45add9cd3617f59
Jonas <jonas@winningmark.com>
2012-06-21 13:53:37 -0700

    full link for images

--- html/protected/extensions/tinymce/TinyMce.php
+++ html/protected/extensions/tinymce/TinyMce.php
@@ -53,6 +53,7 @@ class TinyMce extends CInputWidget {
                     theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
                     theme_advanced_buttons4 : "moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking",
                     theme_advanced_toolbar_location : "top",
+                    relative_urls : false,
                     theme_advanced_toolbar_align : "left",
                     theme_advanced_resizing : true,
                     theme_advanced_font_sizes: "10px,12px,13px,14px,16px,18px,20px, 22px",
