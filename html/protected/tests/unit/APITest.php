b8e47924bc201cab36c4b76398fd7065225bf61a
Jonas <jonas@winningmark.com>
2012-06-22 13:28:45 -0700

    unit tests

--- html/protected/tests/unit/APITest.php
+++ html/protected/tests/unit/APITest.php
@@ -1,48 +1,70 @@
 <?php
+
 Yii::import('application.config.config.php');
 Yii::import('application.shared.models.dal.*');
 Yii::import('application.shared.models.bll.*');
 Yii::import('application.api.models.*');
 
 class APITest extends CDbTestCase {
-        private $device_token;
-        private $uap_user_id;
-        private $type = 'ios';
-        private $district_id;
-        private $optional;
-        
-        public function __construct(){
-            $this->device_token = '120231606E4C8C45F50DA3D0CFB59D78CBE22E0192F63E5A08401BC3BA610232';
-            $this->uap_user_id = 'UwsN1BVESquaXdLA56QzSA';
-            $this->district_id = DistrictManager::getDistrictId('ca', 'congressional', '1');
-            $this->optional = array(
-                'user_agent'=>'firefox',
-    
-            );
-            
-            // delete test user
-            $user = Application_user::model()->findByAttributes(array('device_token'=>$this->device_token));
-            if($user)
-                $user->delete();
-        }
 
-        public function testAddApplicationUser(){
-            $api= new API();
-            
-            $add_user = $api->addApplicationUser($this->device_token, $this->uap_user_id, $this->type, $this->district_id, $this->optional);
-            
-            $this->assertTrue($add_user);
-            
-        }
+    private $device_token;
+    private $uap_user_id;
+    private $type = 'ios';
+    private $district_id;
+    private $optional;
+    private $meta;
+    private $new_tags;
+
+    public function __construct() {
+        $this->device_token = '120231606E4C8C45F50DA3D0CFB59D78CBE22E0192F63E5A08401BC3BA610232';
+        $this->uap_user_id = 'UwsN1BVESquaXdLA56QzSA';
+        $this->district_id = DistrictManager::getDistrictId('ca', 'congressional', '1');
+        $this->optional = array(
+            'user_agent' => 'firefox',
+        );
+
+        $this->meta = array(
+            'name' => 'jonas palmero',
+            'email' => 'jonas@wmse.com'
+        );
+
+        $this->new_tags = array('tag1', 'tag2');
         
-        public function testUpdateApplicationUserMetaByDeviceToken(){
-            $api= new API();
-            
-            $add_user = $api->addApplicationUser($this->device_token, $this->meta);
-            
-            $this->assertTrue($add_user);
-            
-        }
+        $this->optional = array(
+            'meta' => $this->meta,
+            'tags' => $this->new_tags,
+        );
+        
+
+        // delete test user
+        $user = Application_user::model()->findByAttributes(array('device_token' => $this->device_token));
+        if ($user)
+            $user->delete();
+    }
+
+    public function testAddApplicationUser() {
+        $api = new API();
+
+        $add_user = $api->addApplicationUser($this->device_token, $this->uap_user_id, $this->type, $this->district_id, $this->optional);
+
+        $this->assertTrue($add_user);
+    }
+
+    public function testUpdateApplicationUserMetaByDeviceToken() {
+        $api = new API();
+
+        $update_meta = $api->updateApplicationUserMetaByDeviceToken($this->device_token, $this->meta);
+
+        $this->assertTrue($update_meta);
+    }
+
+    public function testUpdateApplicationUserTagsByDeviceToken() {
+        $api = new API();
+
+        $update_meta = $api->updateApplicationUserTagsByDeviceToken($this->device_token, $this->new_tags);
+
+        $this->assertTrue($update_meta);
+    }
 
 }
 
--- html/protected/tests/unit/AppUserTest.php
+++ html/protected/tests/unit/AppUserTest.php
@@ -1,53 +1,71 @@
 <?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+Yii::import('application.config.config.php');
+Yii::import('application.shared.models.dal.*');
+Yii::import('application.shared.models.bll.*');
+Yii::import('application.api.models.*');
 
 class AppUserTest extends CDbTestCase {
-    private $app_user_id = 70;
+
+    private $device_token;
+    private $uap_user_id;
+    private $type = 'ios';
+    private $district_id;
+    private $optional;
+    private $meta;
+    private $new_tags;
+
+    public function __construct() {
+        $this->device_token = '120231606E4C8C45F50DA3D0CFB59D78CBE22E0192F63E5A08401BC3BA610232';
+        $this->uap_user_id = 'UwsN1BVESquaXdLA56QzSA';
+        $this->district_id = DistrictManager::getDistrictId('ca', 'congressional', '1');
+        $this->optional = array(
+            'user_agent' => 'firefox',
+        );
+
+        $this->meta = array(
+            'name' => 'jonas palmero',
+            'email' => 'jonas@wmse.com'
+        );
+
+        $this->new_tags = array('tag1', 'tag2');
+        
+        $this->optional = array(
+            'meta' => $this->meta,
+            'tags' => $this->new_tags,
+        );
+        
+
+        // delete test user
+        $user = Application_user::model()->findByAttributes(array('device_token' => $this->device_token));
+        if ($user)
+            $user->delete();
+    }
+
+    public function testAddApplicationUser() {
 
 
-    
-    public function testGetInvalidTag() {
-        $result = Application_user::model()->findByPk(70)->getMeta('ta41g', true);
-        $this->assertEquals(false, $result);
+        $add_user =  ApplicationUserManager::addApplicationUser($this->device_token, $this->uap_user_id, $this->type, $this->district_id, $this->optional);
+
+        $this->assertTrue($add_user);
     }
+
+    public function testUpdateApplicationUserMetaByDeviceToken() {
     
-    
-    public function testAddMeta(){
-        $meta_key = 'FOO';
-        $meta_value = 'BAR';
-          
-        $add_result = Application_user::model()->findByPk($this->app_user_id)->addMeta($meta_key, $meta_value);
-        $this->assertEquals(true, $add_result);
+
+        $update_meta = ApplicationUserManager::updateApplicationUserMetaByDeviceToken($this->device_token, $this->meta);
+
+        $this->assertTrue($update_meta);
     }
-    
+
+    public function testUpdateApplicationUserTagsByDeviceToken() {
  
-    public function testUpdateSingleMeta(){
-        $update_result = Application_user::model()->findByPk($this->app_user_id)->updateMeta('FOO','ok', 'BAR');
-        $this->assertEquals(true, $update_result);
+
+        $update_meta =  ApplicationUserManager::updateApplicationUserTagsByDeviceToken($this->device_token, $this->new_tags);
+
+        $this->assertTrue($update_meta);
     }
-    
-   public function testGetSingleUserMeta() {
-        $result = Application_user::model()->findByPk(70)->getMeta('FOO',  true);
-        $this->assertEquals('ok', $result['meta_value']);
-    }
-    
-   public function testGetAllUserMeta() {
-        $result = Application_user::model()->findByPk(70)->getMeta('FOO', false);
-        $this->assertEquals('ok', $result[0]['meta_value']);
-    }
-    
-    public function testDeletMeta(){
-        $delete_result = Application_user::model()->findByPk($this->app_user_id)->deleteMeta('FOO', 'ok');
-        $this->assertEquals(true, $delete_result);
-    }
-    
-    
-    public function testUpdateNewMeta(){
-        $update_result = Application_user::model()->findByPk(70)->updateMeta('name', 'jonas palmeeero');
-        $this->assertEquals(true, $update_result);
-    }
-    
+
 }
+
+?>
