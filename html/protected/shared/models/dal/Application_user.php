d0195383ef2743b52670984dc16dc9a24507deef
Jonas <jonas@winningmark.com>
2012-06-25 09:37:28 -0700

    beforeSave: removed uap sync

--- html/protected/shared/models/dal/Application_user.php
+++ html/protected/shared/models/dal/Application_user.php
@@ -55,8 +55,33 @@ class Application_user extends CActiveRecord {
             array('latitude, longitude, registration, type, tags', 'safe'),
             array('registration', 'date', 'format' => 'yyyy-M-d H:m:s'),
             array('id, device_token, latitude, longitude, state_abbr, district_id, district_type, district_number, registration, type, user_agent', 'safe', 'on' => 'search'),
+            array('geolocation', 'geolocation'), // test lat and long format
+            array('type', 'device_type'),
         );
     }
+
+    /**
+     * Validation rules - test that the given lat and long data is a valid format
+     * Set  error field if the lat and long doesnt match a valid format
+     */
+    public function geolocation() {
+        if (isset($this->latitude) && isset($this->longitude)) {
+
+            if (!preg_match('/^[-+]?[0-9]*\,?[0-9]+$/', $this->latitude) || !preg_match('/^[-+]?[0-9]*\,?[0-9]+$/', $this->longitude)) {
+                $this->addError("latlong", 'wrong format');
+            }
+        }
+    }
+    
+    /**
+     * Validation rules - test the device type
+     */
+    public function device_type(){
+        if(!in_array($this->type, self::$device_types))
+                $this->addError('type', 'invalid device type');
+    }
+    
+    
 
     /**
      * @return array relational rules.
@@ -151,259 +176,23 @@ class Application_user extends CActiveRecord {
         if (!$this->longitude)
             $this->longitude = NULL;
 
-        // synchronize tags with urban airship
-        $this->synchronizeUAPTags();
-
         return parent::beforeSave();
     }
 
-    /**
-     * Find  user meta
-     * @param string $meta_key meta key
-     * @param boolean $unique if true, only returns one record, if false, returns all match
-     * $param integer $app_user_id application user id
-     * @return Meta value or false.
+    /*
+     * Attach external behaviors 
+     * @method void test()
      */
-    public function getMeta($meta_key, $unique = false, $app_user_id = null) {
-        if (empty($app_user_id))
-            $app_user_id = $this->id;
 
-        $meta_query = Yii::app()->db->createCommand()
-                ->select('id, meta_key, meta_value')
-                ->from('app_user_meta')
-                ->where('app_user_id=:app_user_id AND meta_key=:meta_key', array(':app_user_id' => $app_user_id, ':meta_key' => $meta_key));
-
-        if ($unique == true) {
-            $result = $meta_query->queryRow();
-        } else {
-            $result = $meta_query->queryAll();
-        }
-
-        if (empty($result))
-            return false;
-        else
-            return $result;
-    }
-
-    /**
-     * Get all the meta data associated to an app user
-     * $param integer $app_user_id application user id
-     * @return array of metas
-     */
-    public function getAllMeta($app_user_id = null) {
-
-        if (empty($app_user_id))
-            $app_user_id = $this->id;
-
-        $meta_query = Yii::app()->db->createCommand()
-                ->select('id, meta_key, meta_value')
-                ->from('app_user_meta')
-                ->where('app_user_id=:app_user_id', array(':app_user_id' => $app_user_id));
-
-        return $meta_query->queryAll();
-    }
-
-    /**
-     * Add  an app user meta data
-     * @param string $meta_key meta key
-     * @param string $meta_value meta meta_value
-     * $param integer $app_user_id application user id
-     * @return true for success. False for failure.
-     */
-    public function addMeta($meta_key, $meta_value, $app_user_id = null) {
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-
-        if (empty($app_user_id))
-            $app_user_id = $this->id;
-
-
-        $meta_add_result = $command->insert('app_user_meta', array(
-            'app_user_id' => $app_user_id,
-            'meta_key' => $meta_key,
-            'meta_value' => $meta_value,
-                ));
-
-
-        if ($meta_add_result > 0) {
-            return true;
-        }
-        else
-            return false;
-    }
-
-    /**
-     * Update an app user meta data
-     * @param string $meta_key meta key
-     * @param string $meta_value meta meta_value
-     * @param string $existing_meta_value  existing_meta_value
-     * $param integer $app_user_id application user id
-     * @return true for success. False for failure.
-     */
-    public function updateMeta($meta_key, $meta_value, $existing_meta_value = null, $app_user_id = null) {
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-
-        if (empty($app_user_id))
-            $app_user_id = $this->id;
-
-        // meta key doesn't exist, create a new one.
-        if (!$this->getMeta($meta_key, true)) {
-            return $this->addMeta($meta_key, $meta_value);
-        }
-
-        if (isset($existing_meta_value)) {
-            $meta_update_result = $command->update('app_user_meta', array(
-                'meta_value' => $meta_value,
-                    ), 'app_user_id=:app_user_id AND meta_key=:meta_key AND meta_value=:old_meta_value', array(
-                ':app_user_id' => $app_user_id,
-                ':meta_key' => $meta_key,
-                ':old_meta_value' => $existing_meta_value,
-                    ));
-        } else {
-            $meta_update_result = $command->update('app_user_meta', array(
-                'meta_value' => $meta_value,
-                    ), 'app_user_id=:app_user_id AND meta_key=:meta_key', array(
-                ':app_user_id' => $app_user_id,
-                ':meta_key' => $meta_key,
-                    ));
-        }
-
-
-
-        if ($meta_update_result > 0) {
-            return true;
-        }
-        else
-            return false;
-    }
-
-    /**
-     * Delete an appuser meta data
-     * @param string $meta_key meta key
-     * @param string $meta_value meta meta_value
-     * $param integer $app_user_id application user id
-     * @return true for success. False for failure.
-     */
-    public function deleteMeta($meta_key, $meta_value = null, $app_user_id = null) {
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-
-        if (empty($app_user_id)) {
-            $app_user_id = $this->id;
-        }
-
-        if (isset($meta_value)) {
-
-            $meta_delete_result = $command->delete('app_user_meta', 'app_user_id=:app_user_id AND meta_key=:meta_key AND meta_value=:meta_value', array(':app_user_id' => $app_user_id, ':meta_key' => $meta_key, ':meta_value' => $meta_value));
-        } else {
-
-            $meta_delete_result = $command->delete('app_user_meta', 'app_user_id=:app_user_id AND meta_key=:meta_key', array(':app_user_id' => $app_user_id, ':meta_key' => $meta_key));
-        }
-
-
-        if ($meta_delete_result > 0) {
-            return true;
-        }
-        else
-            return false;
-    }
-
-    /**
-     * Add a tag to an app_user, uses app_user_tag as the joining table
-     * @param integer $tag id of the tag or string $tag name of the tag
-     * @return true for success. False for failure.
-     */
-    public function addTag($tag) {
-
-        if (is_numeric($tag))
-            $tag_id = $tag;
-        elseif (is_string($tag))
-            $tag_id = Tag::model()->getTagId($tag);
-        else
-            return false;
-
-
-        // if the app user already has a tag, returns false;
-        if ($this->findTag($tag_id))
-            return false;
-
-        $connection = Yii::app()->db;
-
-        $command = $connection->createCommand($sql);
-
-        try {
-            $add_tag_result = $command->insert('app_user_tag', array(
-                'app_user_id' => $this->id,
-                'tag_id' => $tag_id
-                    ));
-        } catch (CException $ce) {
-            error_log("Could not add tag to app user_id $this->id" . $ce->getMessage());
-            $add_tag_result = false;
-        }
-
-        return $add_tag_result;
-    }
-
-    public function deleteTag($tag) {
-        if (is_numeric($tag))
-            $tag_id = $tag;
-        elseif (is_string($tag))
-            $tag_id = Tag::model()->getTagId($tag);
-        else
-            return false;
-
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-
-        $delete_tag_result = $command->delete('app_user_tag', 'app_user_id=:app_user_id AND tag_id=:tag_id', array(':app_user_id' => $this->id, ':tag_id' => $tag_id));
-
-        return $delete_tag_result;
-    }
-
-    /**
-     * Check if an application has the specified tag
-     * @param integer $tag id of the tag or string $tag name of the tag
-     * @return tag id for success. False for failure.
-     */
-    public function findTag($tag) {
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-        if (is_numeric($tag))
-            $tag_id = $tag;
-        else
-            $tag_id = Tag::model()->getTagId($tag);
-
-
-        $result = $command->select('tag_id')
-                ->from('app_user_tag')
-                ->where('app_user_id=:app_user_id AND tag_id=:tag_id', array(':app_user_id' => $this->id, ':tag_id' => $tag_id))
-                ->queryRow();
-
-        return $result['tag_id'];
-    }
-
-    /**
-     * Return all the tags associated to a user
-     * @return tag array object
-     */
-    public function getTagsName() {
-        $connection = Yii::app()->db;
-        $command = $connection->createCommand();
-
-        $result = $command->select('name')
-                ->from(array('app_user_tag', 'tag'))
-                ->where('app_user_id=:app_user_id AND tag.id=app_user_tag.tag_id', array(':app_user_id' => $this->id))
-                ->queryAll();
-
-
-
-        // return $result;
-        return array_map(array(&$this, 'extract_first_el'), $result);
-    }
-
-    private function extract_first_el($a) {
-        return $a['name'];
+    public function behaviors() {
+        return array(
+            'metaBehavior' => array(
+                'class' => 'ApplicationUserMetaBehavior',
+            ),
+            'tagBehavior' => array(
+                'class' => 'ApplicationUserTagBehavior',
+            )
+        );
     }
 
     public function synchronizeUAPTags() {
@@ -423,18 +212,6 @@ class Application_user extends CActiveRecord {
 
     public static function is_valid_type($type) {
         return in_array($type, self::$device_types);
-    }
-
-    /*
-     * Attach external behaviors 
-     * @method void test()
-     */
-    public function behaviors() {
-        return array(
-            'metaBehavior' => array(
-                'class' => 'ApplicationUserMetaBehavior',
-            )
-        );
     }
 
 }
