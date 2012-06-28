4f3a9d7d0ad2eb02db96eb7dcb4743dd34488f7d
Jonas <jonas@winningmark.com>
2012-06-28 09:40:33 -0700

    checkme

--- html/protected/admin/views/ballotItem/_form.php
+++ html/protected/admin/views/ballotItem/_form.php
@@ -43,15 +43,7 @@
             echo $form->error($model, 'district_id');
             ?>
             
-            <?php
-            
-            if($model->id){
-                echo '<!-- debug mode:';
-                  echo $model->district->state_abbr;
-                  echo '-->';
-            }
-            
-            ?>
+
         </div>
 
     </div>
