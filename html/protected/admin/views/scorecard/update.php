c59b76456ebe538a7f2c6b46d8c9bf97a920a54c
Jonas <jonas@winningmark.com>
2012-07-09 10:33:01 -0700

    add new scorecard

--- html/protected/admin/views/scorecard/update.php
+++ html/protected/admin/views/scorecard/update.php
@@ -1,5 +1,4 @@
 <?php
-
 $this->breadcrumbs = array(
     'Ballot Items' => array('ballotItem/index'),
     $model->ballotItem->item => array('ballotItem/update', 'id' => $model->ballotItem->id),
@@ -8,12 +7,12 @@ $this->breadcrumbs = array(
 
 $this->menu = array(
     array('label' => 'Delete this scorecard item', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this scorecard item?')),
+    array('label' => 'Adde another scorecard item', 'url' => CHtml::normalizeUrl(array('scorecard/add', 'ballot_item_id' => $model->ballotItem->id))),
 );
 ?>
 
 <h1>Update a scorecard item for: <?php echo $model->ballotItem->item; ?> </h1>
 
 <?php
-
-echo $this->renderPartial('_form', array('model' => $model  ));
+echo $this->renderPartial('_form', array('model' => $model));
 ?>
