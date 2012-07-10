65c9b0cc4382bb6752d3ffe637dde4d503ef676e
Jonas <jonas@winningmark.com>
2012-07-10 09:24:48 -0700

    new extension

---
+++ html/protected/extensions/AjaxFileUploader/AjaxFileUploader.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class AjaxFileUploader extends CWidget {
+
+    public $options = array();
+    public $model;
+    public $attribute;
+
+    public function start() {
+        $attribute = $this->attribute;
+
+        $img_url = $this->model->$attribute;
+
+        if ($img_url)
+            $this->displayThumb($img_url);
+
+        $this->displayModalUploaderButton();
+    }
+
+    public function init() {
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();
+
+
+        $cs->registerScriptFile($baseUrl . '/js/form/jquery.form.js');
+    }
+
+    private function displayThumb($image_url) {
+        echo '<div><img id="'.$this->attribute.'_image_preview" src="' . $image_url . '" style="width:50px; height:50px; "/></div>';
+    }
+
+    private function displayModalUploaderButton() {
+
+        echo CHtml::activeTextField($this->model, $this->attribute, array('id' => 'upload_insert_image'));
+        ?>
+        <div>
+            <input type="button" id="opener" class="btn btn-primary" value="Upload"></input>
+        </div>
+
+
+        <script type="text/javascript">
+                                                                                                                                                        
+            jQuery(document).ready(function($){
+                                                                
+                image_dialog =  $("#dialog").dialog({
+                    autoOpen: false,
+                    modal: true,
+                    height: 450,
+                    width: 700,
+                    title: "Upload an image",
+                    buttons:{
+                        Close: function() {
+                            $( this ).dialog( "close" );
+                        } 
+                    }
+                });
+                                                                                                                                 
+                $('#opener').click(function(){           
+                    image_dialog.load('<?php echo $this->options['modal_view']; ?>' ).dialog("open");                                   
+                });
+            });
+                                                                                                                                                
+        </script>
+
+
+        <div id="dialog">loading</div>
+
+        <?php
+    }
+    
+    
+    public function modalWindow(){
+        $this->render('ModalWindow');
+    }
+
+}
