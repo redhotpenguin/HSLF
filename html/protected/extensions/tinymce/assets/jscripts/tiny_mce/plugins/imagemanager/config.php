7407caee1885619538b6030dc35644c30dabde47
Jonas <jonas@winningmark.com>
2012-07-13 12:52:31 -0700

    security fix

--- html/protected/extensions/tinymce/assets/jscripts/tiny_mce/plugins/imagemanager/config.php
+++ html/protected/extensions/tinymce/assets/jscripts/tiny_mce/plugins/imagemanager/config.php
@@ -2,10 +2,22 @@
 
 require_once(MCMANAGER_ABSPATH . "ImageManager/ImageManagerPlugin.php");
 
+@session_start();
+// ugliest hack ever
+$connected = false;
+foreach ($_SESSION as $t) {
+    if (is_array($t)) {
+        if ($t['role'] == 1)
+            $connected = true;
+    }
+}
+
 if ($connected == false) {
     echo 'Incident reported: ' . $_SERVER['REMOTE_ADDR'] . ' tried to access a secured area without proper privileges.';
 
     error_log($_SERVER['REMOTE_ADDR'] . " tried to access the image manager without proper authorization.");
+
+    exit;
 }
 
 
