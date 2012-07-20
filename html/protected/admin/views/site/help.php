783a769d9073498cae5bbb1b2d43036e7a862d9d
Jonas <jonas@winningmark.com>
2012-07-20 13:11:43 -0700

    help view

---
+++ html/protected/admin/views/site/help.php
@@ -0,0 +1,33 @@
+<header class="jumbotron subhead" id="overview">
+
+  <h1>Dashboard Help</h1><br/>
+  <div class="subnav">
+    <ul class="nav">
+      <li class=""><h3><a href="#BallotItems">Ballot items</a></h3></li>
+      <li class=""><h3><a href="#ImageUpload">Image Upload</a></h3></li>
+      <li class=""><h3><a href="#pushNotifications">Push Notifications</a></h3></li>
+    </ul>
+  </div>
+</header>
+
+
+<hr/>
+
+
+<h3>Ballot items</h3>
+
+<p id="BallotItems">Coming soon.</p>
+    
+<hr/>
+
+<h3>Image Upload</h3>
+<p id="ImageUpload">Coming soon.</p>
+    
+<hr/>
+
+<h3>Push Notifications</h3>
+<p id="pushNotifications">
+Coming soon.
+</p>
+    
+
