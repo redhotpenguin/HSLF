93cb3b729d6aa65d16951f9933c6277a26aecb95
Jonas <jonas@winningmark.com>
2012-08-03 16:46:36 -0700

    cron.php

---
+++ cron.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require_once(dirname(__FILE__).'/html/protected/config/config.php');
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/framework/yii.php';
+$config=dirname(__FILE__).'/html/protected/config/cron.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+
+$app = Yii::createConsoleApplication($config);
+
+$app->run();
