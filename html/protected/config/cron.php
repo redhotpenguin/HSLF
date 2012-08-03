8f5489ad337191bc066434436348ba6ff7a37ea2
Jonas <jonas@winningmark.com>
2012-08-03 16:48:06 -0700

    cron conf file

---
+++ html/protected/config/cron.php
@@ -0,0 +1,79 @@
+<?php
+
+// frontend config
+
+return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => PROJECT_NAME,
+    // preloading 'log' component
+    'preload' => array('log'),
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.shared.models.dal.*', // data access logic classes
+        'application.shared.models.bll.*', // business  logic classes
+        'application.components.*',
+        'ext.multimodelform.MultiModelForm',
+    ),
+    'modules' => array(
+    /*
+      'gii' => array(
+      'class' => 'system.gii.GiiModule',
+      'password' => 'giipass',
+      // If removed, Gii defaults to localhost only. Edit carefully to taste.
+      'ipFilters' => array('127.0.0.1', '::1'),
+      ),
+     */
+    ),
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ),
+        // uncomment the following to enable URLs in path-format
+
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'showScriptName' => false,
+            'rules' => array(
+            /*
+              array('ballot/listbydistrict', 'pattern' => 'ballot/<year:\d{4}>/<state_abbr:\w{2,3}>/<district_type:\w{2,15}>/<district:\w+>', 'verb' => 'GET'), // /ballot/2012/ca/congressional/14
+              array('ballot/view', 'pattern' => 'ballot/<year:\d{4}>/<slug:.*?>', 'verb' => 'GET'), //  /ballot/2012/puppy-mills/
+              array('ballot/list', 'pattern' => 'ballot/<state_abbr:\w{2,3}>', 'verb' => 'GET'), //  /ballot/ca
+              array('ballot/list', 'pattern' => 'ballot/<year:\d{4}>/<state_abbr:\w{2,3}>/<district_type:\w{2,15}>', 'verb' => 'GET'), // /ballot/ca/congressional/
+             */
+            ),
+        ),
+        'db' => array(
+            'connectionString' => "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME,
+            'emulatePrepare' => true,
+            'username' => DB_USER,
+            'password' => DB_PASS,
+            'charset' => 'UTF-8',
+            'schemaCachingDuration' => '3600',
+        ),
+        'cache' => array(
+            'class' => 'system.caching.CApcCache',
+        ),
+        'errorHandler' => array(
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+
+                array(
+                    'class' => 'CWebLogRoute',
+                ),
+            ),
+        ),
+    ),
+
+ 
+);
