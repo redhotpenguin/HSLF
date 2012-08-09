77b6be0575f5c62039373080a41c154ae6d1d0e1
Jonas <jonas@winningmark.com>
2012-08-09 11:39:27 -0700

    update

--- html/protected/admin/controllers/BallotItemController.php
+++ html/protected/admin/controllers/BallotItemController.php
@@ -95,9 +95,10 @@ class BallotItemController extends Controller {
      * @param integer $id the ID of the model to be updated
      */
     public function actionUpdate($id) {
-        //   error_log(print_r($_REQUEST, true ));
         // import FileUpload helper class
         Yii::import('admin.models.helpers.FileUpload');
+
+        // error_log (  print_r($_POST, true )  );
 
         $model = $this->loadModel($id);
 
@@ -139,6 +140,48 @@ class BallotItemController extends Controller {
                 if ($model->save())
                     $this->redirect(array('update', 'id' => $model->id, 'updated' => true));
             }
+            // savescorecards
+
+            if ($scorecard_item_ids = getPost('scorecards')) {
+
+
+                error_log(print_r($scorecard_item_ids, true));
+
+
+                $scorecard_model = new Scorecard();
+                foreach ($scorecard_item_ids as $scorecard_item_id => $vote_id) {
+
+                    $scorecard = $scorecard_model->findByAttributes(array(
+                        "ballot_item_id" => $id,
+                        "scorecard_item_id" => $scorecard_item_id
+                            ));
+
+                    if ($scorecard) { // update or delete existing scorecard 
+                        if (!$vote_id)
+                            $scorecard->delete();
+
+
+                        $scorecard->vote_id = $vote_id;
+                        $scorecard->save();
+                    } else { // insert new scorecard
+                        if (!$vote_id)
+                            continue;
+
+                        $scorecard_model = new Scorecard();
+                        $scorecard_model->attributes = array('ballot_item_id' => $id, 'scorecard_item_id' => $scorecard_item_id, 'vote_id' => $vote_id);
+                        $scorecard_model->save();
+                    }
+
+
+
+                    // $command->reset();
+                    // delete existing scorecarditems
+                    //   $command->reset();
+                    // insert new scorecard items
+                    // $command->insert('scorecard', array('ballot_item_id' => $id, 'scorecard_item_id' => $scorecard_item_id, 'vote_id' => $vote_id));
+                }
+            }
+
 
             return;
         }
