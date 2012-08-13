49394a2438cc81c5044c09c8dd14d948b39b7d97
Jonas <jonas@winningmark.com>
2012-08-13 13:58:33 -0700

    removed leftovers

--- html/protected/api/views/api/issue/issue_detail.php
+++
@@ -1,8 +0,0 @@
-<?php
-//the output of this file will be presented in the issue detail payload
-
-// candidate_issue structure: name,  value, detail
-// accessing to the original candidate: $candidate_issue->candidate->full_name
-
-
- echo $candidate_issue->candidate->full_name.' has voted '. $candidate_issue->value .' on ' . $candidate_issue->name;
