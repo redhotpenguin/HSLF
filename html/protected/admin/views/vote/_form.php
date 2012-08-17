e807d1942fc9b48f631d802fcd3af064c522ab19
Jonas <jonas@winningmark.com>
2012-08-17 15:42:00 -0700

    vote updates

--- html/protected/admin/views/vote/_form.php
+++ html/protected/admin/views/vote/_form.php
@@ -1,29 +1,31 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'vote-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'vote-form',
+        'enableAjaxValidation' => false,
+            ));
+    ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+        <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>64)); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'name'); ?>
+<?php echo $form->textField($model, 'name', array('size' => 60, 'maxlength' => 64, 'class' => 'span9')); ?>
+<?php echo $form->error($model, 'name'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'icon'); ?>
-		<?php echo $form->textArea($model,'icon',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'icon'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'icon'); ?>
+<?php echo $form->textField($model, 'icon', array('size' => 60, 'maxlength' => 64, 'class' => 'span9')); ?>
+<?php echo $form->error($model, 'icon'); ?>
+    </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
+    <div class="row buttons">
+    <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
 
 <?php $this->endWidget(); ?>
 
