0be76924b133c7955e1e646de0c9e88aa15c7e7f
Jonas <jonas@winningmark.com>
2012-09-10 09:51:19 -0700

    endorser view updates

--- html/protected/admin/views/endorser/view.php
+++ html/protected/admin/views/endorser/view.php
@@ -1,26 +1,42 @@
 <?php
-$this->breadcrumbs=array(
-	'Endorsers'=>array('index'),
-	$model->name,
+$this->breadcrumbs = array(
+    'Endorsers' => array('index'),
+    $model->name,
 );
 
-$this->menu=array(
-	array('label'=>'Add an endorser', 'url'=>array('create')),
-	array('label'=>'Update endorser', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete endorser', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage endorsers', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Add an endorser', 'url' => array('create')),
+    array('label' => 'Update endorser', 'url' => array('update', 'id' => $model->id)),
+    array('label' => 'Delete endorser', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+    array('label' => 'Manage endorsers', 'url' => array('admin')),
 );
 ?>
 
 <h1>View Endorser #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'name',
-		'description',
-		'website',
-		'image_url',
-	),
-)); ?>
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+    'data' => $model,
+    'attributes' => array(
+        'id',
+        'name',
+        'description',
+        array(
+            'name' => 'website',
+            'type' => 'raw',
+            'value' => CHtml::link($model->website, $model->website, array('target' => '_blank')),
+        ),
+        array(
+            'name' => 'image_url',
+            'type' => 'raw',
+            'value' => CHtml::link($model->image_url, $model->image_url, array('target' => '_blank')),
+        ),
+    ),
+));
+
+
+if ($model->image_url)
+    echo "<img src='{$model->image_url}' />";
+?>
+
+
