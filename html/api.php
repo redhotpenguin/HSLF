45d9dbf722de6801419d05b983993a9f2163c552
Jonas Palmero <jonas@winningmark.com>
2012-09-10 16:01:00 -0700

    API: turned off debug mode

--- html/api.php
+++ html/api.php
@@ -9,10 +9,10 @@ require_once(dirname(__FILE__).'/protected/config/config.php');
 $config=dirname(__FILE__).'/protected/api/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true );
+defined('YII_DEBUG') or define('YII_DEBUG', false );
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-defined('YII_DEBUG_SHOW_PROFILER') or define('YII_DEBUG_SHOW_PROFILER',true);
+defined('YII_DEBUG_SHOW_PROFILER') or define('YII_DEBUG_SHOW_PROFILER',false);
 
 require_once($yii);
 
