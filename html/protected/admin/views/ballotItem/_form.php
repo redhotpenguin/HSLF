679bc8997d678740d06cf216e5144c6a8bf66495
Jonas Palmero <jonas@winningmark.com>
2012-09-13 09:04:43 -0700

    not applicable

--- html/protected/admin/views/ballotItem/_form.php
+++ html/protected/admin/views/ballotItem/_form.php
@@ -310,7 +310,7 @@ $cs->registerScriptFile($baseUrl . '/themes/dashboard/js/form/ballotItem.js');
     <div class="right_col">
         <?php
         echo $form->labelEx($model, 'hold_office');
-        echo $form->dropDownList($model, 'hold_office', array('no' => 'No', 'yes' => 'Yes', ));
+        echo $form->dropDownList($model, 'hold_office', array('na'=>'Not Applicable', 'no' => 'No', 'yes' => 'Yes', ));
         echo $form->error($model, 'hold_office');
         ?>
     </div>
