71adcdfedfc614fe3eab0733d8c860dac230a30a
Jonas Palmero <jonas@winningmark.com>
2012-09-13 10:22:51 -0700

    api v2 conf

--- html/protected/config/config.php
+++ html/protected/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
+//$env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
 
 if (isset($env['DOTCLOUD_DB_SQL_HOST']))
     $dbhost = $env['DOTCLOUD_DB_SQL_HOST'];
@@ -37,7 +37,7 @@ DEFINE('ADMIN_EMAIL', 'mobile@winningmark.com');
 
 
 // DB Config
-DEFINE('DB_NAME', 'ouroregon');
+DEFINE('DB_NAME', 'ouroregon_dev');
 DEFINE('DB_HOST', $dbhost);
 DEFINE('DB_USER', $dbuser);
 DEFINE('DB_PASS', $dbpass);
