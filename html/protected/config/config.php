650ccff3abf95d97823f87f0ab32f8144d6a3387
Jonas Palmero <jonas@winningmark.com>
2012-09-13 09:34:53 -0700

    config

--- html/protected/config/config.php
+++ html/protected/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
+// $env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
 
 if (isset($env['DOTCLOUD_DB_SQL_HOST']))
     $dbhost = $env['DOTCLOUD_DB_SQL_HOST'];
@@ -32,12 +32,13 @@ set_include_path(get_include_path() . PATH_SEPARATOR . '/home/dotcloud/php-env/s
 // please also update root path in protected/extensions/tinymce/assets/jscripts/tiny_mce/plugins/imagemanager/config.php
 // Project config
 DEFINE('PROJECT_NAME', 'Our Oregon');
-DEFINE('SITE_URL', 'http://oo.m.winningmark.com');
+//DEFINE('SITE_URL', 'http://mvg.winningmark.com');
+DEFINE('SITE_URL', 'http://www.voterguide.com');
 DEFINE('ADMIN_EMAIL', 'mobile@winningmark.com');
 
 
 // DB Config
-DEFINE('DB_NAME', 'ouroregon');
+DEFINE('DB_NAME', 'ouroregon_dev');
 DEFINE('DB_HOST', $dbhost);
 DEFINE('DB_USER', $dbuser);
 DEFINE('DB_PASS', $dbpass);
@@ -48,7 +49,7 @@ DEFINE('API_KEY', '52356');
 DEFINE('API_SECRET', 'PqiW_IDKL3mFi_OirCqOe-u');
 
 // Urban Airship
-DEFINE('UA_API_KEY', '3rQVdPPdT7Osvdx1vB37Tg');
+DEFINE('UA_API_KEY', '3rQVdPPdT7Osvdx1vB37Tg'); // mvg-platform credentials
 DEFINE('UA_API_SECRET', 'X9k8V7YpQzi4cwkviE_RXA'); // master secret
 DEFINE('UA_DASHBOARD_LINK', 'https://go.urbanairship.com/apps/3rQVdPPdT7Osvdx1vB37Tg/composer/rich-push/');
 
