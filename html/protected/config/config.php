40d8b78f9cc27129c2494f1a37474ee87d6d3406
Jonas Palmero <jonas@winningmark.com>
2012-09-13 11:15:37 -0700

    cross environment conf file

--- html/protected/config/config.php
+++ html/protected/config/config.php
@@ -1,29 +1,25 @@
 <?php
- $env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
 
-if (isset($env['DOTCLOUD_DB_SQL_HOST']))
+$dotcloud_conf = "/home/dotcloud/environment.json";
+
+if (file_exists($dotcloud_conf)) {     // prod server conf
+    $env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
     $dbhost = $env['DOTCLOUD_DB_SQL_HOST'];
-else
-    $dbhost = 'localhost';
-
-if (isset($env['DOTCLOUD_DB_SQL_LOGIN']))
+    $dbname = 'ouroregon';
     $dbuser = $env['DOTCLOUD_DB_SQL_LOGIN'];
-else
-    $dbuser = 'postgres';
-
-
-if (isset($env['DOTCLOUD_DB_SQL_PASSWORD']))
     $dbpass = $env['DOTCLOUD_DB_SQL_PASSWORD'];
-else
-    $dbpass = 'pengu1n';
-
-
-if (isset($env['DOTCLOUD_DB_SQL_PORT']))
     $dbport = $env['DOTCLOUD_DB_SQL_PORT'];
-else
+    set_include_path(get_include_path() . PATH_SEPARATOR . '/home/dotcloud/php-env/share/php');
+    
+} else {    //dev server conf
+    $dbhost = 'localhost';
+    $dbname = 'ouroregon_dev';
+    $dbuser = 'postgres';
+    $dbpass = 'pengu1n';
     $dbport = '5444';
+}
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '/home/dotcloud/php-env/share/php');
+
 
 
 
@@ -37,7 +33,7 @@ DEFINE('ADMIN_EMAIL', 'mobile@winningmark.com');
 
 
 // DB Config
-DEFINE('DB_NAME', 'ouroregon');
+DEFINE('DB_NAME', $dbname);
 DEFINE('DB_HOST', $dbhost);
 DEFINE('DB_USER', $dbuser);
 DEFINE('DB_PASS', $dbpass);
@@ -55,8 +51,6 @@ DEFINE('UA_DASHBOARD_LINK', 'https://go.urbanairship.com/apps/3rQVdPPdT7Osvdx1vB
 // Cicero Config
 DEFINE('CICERO_USERNAME', 'winningmark');
 DEFINE('CICERO_PASSWORD', '3TUuAv5DwNsB');
-
-
 
 // Theme config
 DEFINE('ADMIN_THEME', 'dashboard');
