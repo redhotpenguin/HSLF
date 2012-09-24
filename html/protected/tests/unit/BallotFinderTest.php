2c46b702b392c3d3d510258a6ed8cd34db6f5969
Jonas <jonas@winningmark.com>
2012-09-24 10:48:33 -0700

    removed priority from the project

--- html/protected/tests/unit/BallotFinderTest.php
+++ html/protected/tests/unit/BallotFinderTest.php
@@ -1,27 +1,22 @@
 <?php
 
-
-
 class BallotFinderTest extends CTestCase {
 
     public function testConstruct() {
         $district_ids = DistrictManager::getIdsByDistrictType('or', 'congressional');
-        
+
 
         $ballotItemFinder = new BallotItemFinder();
-        
-       // $ballotItemFinder->setParty("independant");
-     //   $ballotItemFinder->setParty("N/A");
+
+        // $ballotItemFinder->setParty("independant");
+        //   $ballotItemFinder->setParty("N/A");
         $ballotItemFinder->setDistrictIds($district_ids);
-      //   
+        //   
         $ballotItemFinder->setPublished('yes');
-        
-        $ballotItemFinder->orderByHighestPriority();
-        
-      //  $ballotItemFinder->setRunningOnly();
+
+
+        //  $ballotItemFinder->setRunningOnly();
         $ballotItemFinder->setPublishedYear(2014);
-        
-       // $ballotItemFinder->setPriority(5);
         $ballots = $ballotItemFinder->search();
 
         // error_log(print_r($ballots, true));
