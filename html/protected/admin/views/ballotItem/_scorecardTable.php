028fda4e0e794e44b0896996c5efbc92cf1b5cdc
Jonas <jonas@winningmark.com>
2012-10-04 12:00:03 -0700

    php strict fix for scorecard table

--- html/protected/admin/views/ballotItem/_scorecardTable.php
+++ html/protected/admin/views/ballotItem/_scorecardTable.php
@@ -9,22 +9,24 @@
     $listed_votes = CHtml::listData(Vote::model()->findAll(), 'id', 'name');
     $listed_votes += array('0' => 'Not set');
 
-    foreach (ScorecardItem::model()->findAllByAttributes(array('office_id'=>$office_id)) as $scorecardItem) {
+    foreach (ScorecardItem::model()->findAllByAttributes(array('office_id' => $office_id)) as $scorecardItem) {
         echo '<tr>';
 
         echo '<td>' . $scorecardItem->name . '</td>';
 
         echo '<td>';
 
-        $scorecard = Scorecard::model()->findByAttributes(array(
-            "ballot_item_id" => $model->id,
-            "scorecard_item_id" => $scorecardItem->id
-                ));
 
-        if ($scorecard) {
+        if (isset($model))
+            $scorecard = Scorecard::model()->findByAttributes(array(
+                "ballot_item_id" => $model->id,
+                "scorecard_item_id" => $scorecardItem->id
+                    ));
+
+        if (isset($scorecard)) {
             echo CHtml::dropDownList("scorecards[{$scorecard->scorecardItem->id}]", $scorecard->vote->id, $listed_votes);
         } else {
-             echo CHtml::dropDownList("scorecards[{$scorecardItem->id}]", 0, $listed_votes);
+            echo CHtml::dropDownList("scorecards[{$scorecardItem->id}]", 0, $listed_votes);
         }
 
         echo '</td>';
