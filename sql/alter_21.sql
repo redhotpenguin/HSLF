3bfbb72325288b6f85ddcec408b8a626b21448dc
Fred Moyer <fred@redhotpenguin.com>
2012-10-10 12:36:11 -0700

    add sql

---
+++ sql/alter_21.sql
@@ -0,0 +1,21 @@
+-- 
+-- this is the twentyfirst alter of the database.
+-- dont forget to flush schema cache! Yii::app()->cache->flush();
+-- added postgres function: upsert for option table
+---  STILL IN DEV
+
+
+-- ballot item news slug
+ALTER TABLE ballot_item_news ADD COLUMN slug VARCHAR(512);
+
+UPDATE ballot_item_news SET slug = LOWER( REPLACE( title ,' ', '-') ) ;
+
+ALTER TABLE ballot_item_news ALTER COLUMN slug SET NOT NULL;
+
+
+-- category field for alert_type
+ALTER TABLE alert_type ADD COLUMN category VARCHAR(512);
+
+UPDATE alert_type SET category = 'news';
+
+ALTER TABLE alert_type ALTER COLUMN category SET NOT NULL;
