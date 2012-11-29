078edc40792ec66ee5e0bf918b045a13e6c67be8
Jonas <jonas@winningmark.com>
2012-11-29 15:49:45 -0800

    base active record

---
+++ html/protected/shared/models/dal/TenantCActiveRecord.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TenantCActiveRecord
+ *
+ * @author jonas
+ */
+class TenantCActiveRecord extends CActiveRecord {
+    //put your code here
+}
+
+?>
