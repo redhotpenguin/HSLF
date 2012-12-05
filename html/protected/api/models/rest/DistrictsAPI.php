912c3b4dbb411ee1d0dfa8b4e9430e0a35167cca
Jonas <jonas@winningmark.com>
2012-12-05 09:46:09 -0800

    removedu unused API rest models

--- html/protected/api/models/rest/DistrictsAPI.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class DistrictsAPI extends APIBase implements IAPI {
-
-    public function __construct(){
-        parent::__construct( new District );
-    }
-}
--- html/protected/api/models/rest/StatesAPI.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class StatesAPI extends APIBase {
-
-    public function __construct(){
-        parent::__construct( new State );
-    }
-
-}
