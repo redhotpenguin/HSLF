2c09cfd517eb4416534b015f4ace97f5fe1432b5
Jonas <jonas@winningmark.com>
2012-12-05 11:19:42 -0800

    unit test for option behaviors

---
+++ html/protected/tests/unit/OptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class OptionTest extends CDbTestCase {
+
+    private $option;
+    private $optionBehavior;
+    private $tenantBehavior;
+
+    public function __construct() {
+        $this->option = new Option();
+        $this->optionBehavior = new OptionBehavior();
+        $this->tenantBehavior = new MultiTenantBehavior();
+
+        $this->option->attachBehavior('Behavior', $this->optionBehavior);
+        $this->option->attachBehavior('MultiTenant', $this->tenantBehavior);
+        
+        $this->option->sessionTenantId = 1;
+    }
+
+    public function testUpsert() {
+
+        $result = $this->option->upsert("foo", "bar");
+         $result = $this->option->upsert("foo", "bar41");
+
+        error_log("result: $result");
+
+        $this->assertTrue($result);
+    }
+
+}
+
+?>
