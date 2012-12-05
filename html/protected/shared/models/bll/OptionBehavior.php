b54b1a320795af3ae2de15790b9bcb87887d47b3
Jonas <jonas@winningmark.com>
2012-12-05 11:19:55 -0800

    unit test for option behaviors

---
+++ html/protected/shared/models/bll/OptionBehavior.php
@@ -0,0 +1,22 @@
+<?php
+
+class OptionBehavior extends CActiveRecordBehavior {
+
+    public function upsert($name, $value) {
+
+        $option = $this->owner->findByAttributes(array("name" => $name));
+
+        if ($option == null) { // insert
+            error_log("insert");
+            $this->owner->name = $name;
+            $this->owner->value = $value;
+            return $this->owner->save();
+        } else { // update
+            $option->value = $value;
+            return $option->save();
+        }
+    }
+
+}
+
+?>
