83a14eac3953f7ae5ecc508c73c6da6a6af51ae5
Jonas <jonas@winningmark.com>
2012-12-18 11:15:19 -0800

    mobile user dashboard integration

---
+++ html/protected/admin/controllers/MobileUsersController.php
@@ -0,0 +1,51 @@
+<?php
+
+class MobileUsersController extends Controller {
+
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
+    public $category = array( 'Application Manager'=>array('/site/mobile/')); // used by the breadcrumb
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow',
+                'actions' => array('index'),
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $dataProvider = new CActiveDataProvider('AlertType');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
+
+    
+
+
+}
