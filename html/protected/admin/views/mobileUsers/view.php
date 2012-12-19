2d38f70519058bb875e67f2c5dd9c39ee64c90ff
Jonas <jonas@winningmark.com>
2012-12-19 14:25:31 -0800

    updates

--- html/protected/admin/views/mobileUsers/view.php
+++ html/protected/admin/views/mobileUsers/view.php
@@ -1,10 +1,53 @@
 <?php
-
 $this->breadcrumbs = array(
     'Mobile Users' => array('index'),
     $model->_id,
 );
+?>
 
-echo '<pre>';
-print_r($model->fields);
-echo '</pre>';
+
+<table>
+
+    <tbody class="table">
+        <?php
+        foreach ($model->fields as $field => $value) {
+            ?>
+            <tr>
+                <td><?php echo $field; ?></td>
+                <td><?php
+        if (is_array($value)) {
+            foreach ($value as $v) {
+                echo $v . ',';
+            }
+        } elseif (is_object($value)) {
+            if ($value instanceof MongoId) {
+                echo $value->{'$id'};
+            } elseif ($value instanceof MongoDate) {
+                echo $value->sec;
+            } else {
+                print_r($value);
+            }
+        } else {
+            echo $value;
+        }
+            ?></td>
+
+
+            </tr>
+
+            <?php
+        }
+        ?>
+    </tbody>
+
+</table>
+
+
+
+
+<?php
+if (YII_DEBUG):
+    echo 'Debug:<pre>';
+    print_r($model->fields);
+    echo '</pre>';
+endif;
--- html/protected/shared/models/dal/MobileUser.php
+++ html/protected/shared/models/dal/MobileUser.php
@@ -75,5 +75,6 @@ class MobileUser extends ActiveMongoDocument {
         );
     }
 
+
 }
 
