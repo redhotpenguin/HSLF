ed42fc70c1aa836ae37d8f3e40425c61aab1b536
Jonas <jonas@winningmark.com>
2012-12-20 09:55:08 -0800

    renamed mobile user controller

--- html/protected/admin/controllers/MobileUserController.php
+++ html/protected/admin/controllers/MobileUserController.php
@@ -1,6 +1,6 @@
 <?php
 
-class MobileUsersController extends Controller {
+class MobileUserController extends Controller {
 
     /**
      * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
--- html/protected/admin/views/site/index.php
+++ html/protected/admin/views/site/index.php
@@ -9,7 +9,7 @@
             <h1>Dashboard</h1>
             <p>Welcome to the administration dashboard.</p>
             <p> <b><?php echo $total_item_number; ?></b> <a href="/admin/item/admin/">Items</a></p>
-            <p> <b><?php echo $total_user_number; ?></b> <a href="/admin/mobileUsers/">Mobile Users</a></p>
+            <p> <b><?php echo $total_user_number; ?></b> <a href="/admin/mobileUser/">Mobile Users</a></p>
 
         </div>
 
