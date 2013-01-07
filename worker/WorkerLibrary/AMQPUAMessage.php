56379e995e262159de04138c768b5e1336ddad38
Jonas Palmero <jonas@winningmark.com>
2013-01-07 12:38:18 -0800

    new fields + documentation

--- worker/WorkerLibrary/AMQPUAMessage.php
+++ worker/WorkerLibrary/AMQPUAMessage.php
@@ -9,11 +9,53 @@ namespace WorkerLibrary;
  */
 class AMQPUAMessage {
 
+    /**
+     * Client Name
+     * @var string 
+     */
+    public $clientName;
+
+    /**
+     * Client Email
+     * @var string 
+     */
+    public $clientEmail;
+
+    /**
+     * Urban Airship API Key
+     * @var string 
+     */
+    public $apiKey;
+
+    /**
+     * Urban Airship API Secret
+     * @var string 
+     */
+    public $apiSecret;
+
+    /**
+     * array of tokens (ios)
+     * @var array 
+     */
     public $tokens = array();
+
+    /**
+     * array of apids (androids)
+     * @var array 
+     */
     public $apids = array();
+
+    /**
+     * array of key/value pairs
+     * @var array 
+     */
     public $customData = array();
-    
-    // helpers
+
+    /**
+     * Helper - Unserialize a message
+     * @param string  $serializedMsg serialized messsage
+     * @return AMQPUAMessage AMQPUAMessage object
+     */
     public static function unserialize($serializedMsg) {
         $amqpUaMessage = unserialize($serializedMsg);
         if ($amqpUaMessage == false) {
@@ -22,8 +64,12 @@ class AMQPUAMessage {
             return $amqpUaMessage;
         }
     }
-    
-    public function serialize(){
+
+    /**
+     * Helper - serialize a message
+     * @return string serialized AMQPUAMessage object
+     */
+    public function serialize() {
         return serialize($this);
     }
 
