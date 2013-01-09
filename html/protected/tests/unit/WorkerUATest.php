64fcbcd1b2a72884bf0041f53f1c2cf075e6d155
Jonas Palmero <jonas@winningmark.com>
2013-01-09 11:03:26 -0800

    use allow-dev for pushes

--- html/protected/tests/unit/WorkerUATest.php
+++ html/protected/tests/unit/WorkerUATest.php
@@ -9,20 +9,24 @@ class WorkerUATest extends CDbTestCase {
     private $airship;
 
     public function __construct() {
-       
-        $this->airship = new UrbanAirship("G3QQPQEERdKchSqDPq6Gag", "FT98LRhLRNOPHBg8k-5iyg");
+
+        //$this->airship = new UrbanAirship("G3QQPQEERdKchSqDPq6Gag", "FT98LRhLRNOPHBg8k-5iyg"); // our oregon dev
+        
+        
+        $this->airship = new UrbanAirship("3ZdPxcFfSda0rpWtlwE68w", "42YO18MlSBC6JC-ewFoK2w"); // alloy dev
     }
-    
+
     // directly use the uap library
     public function testSendPushNotification() {
         $apids = array(
             "5d1cf0cb-90ce-4960-8c12-adc8c8bec35e"
         );
 
-        $result = $this->airship->sendPushNotification("Bonjour Jonas!!", null, $apids);
+        $tokens = array( "120231606E4C8C45F50DA3D0CFB59D78CBE22E0192F63E5A08401BC3BA610232" ) ; // dev iphone
+
+        $result = $this->airship->sendPushNotification("Bonjour Jonas!!", $tokens, null);
 
         $this->assertTrue($result);
-
     }
 
 }
