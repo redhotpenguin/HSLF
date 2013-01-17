89c16b5d97ea1905ce7f9f756c166dc9406813aa
Jonas Palmero <jonas@winningmark.com>
2013-01-17 09:00:17 -0800

    updates

--- html/admin.php
+++ html/admin.php
@@ -8,7 +8,7 @@ require_once(dirname(__FILE__).'/protected/config/config.php');
 $config=dirname(__FILE__).'/protected/admin/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true );
+//defined('YII_DEBUG') or define('YII_DEBUG', true );
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 //defined('YII_DEBUG_SHOW_PROFILER') or define('YII_DEBUG_SHOW_PROFILER',true);
