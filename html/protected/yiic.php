ae7663756d51b8b5c9c06d99b5a34edd8ada2e6c
Jonas Palmero <jonas@winningmark.com>
2013-01-22 09:56:04 -0800

    path updates

--- html/protected/yiic.php
+++ html/protected/yiic.php
@@ -1,7 +1,11 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../framework/yiic.php';
+
+$yiic=dirname(__FILE__).'/../../framework/yiic.php';
+
+dirname(__FILE__).'/config/console.php';
+
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
