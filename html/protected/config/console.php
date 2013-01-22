a2d3a6dcf58b012be629b947b77af1058191863d
Jonas Palmero <jonas@winningmark.com>
2013-01-22 10:10:33 -0800

    conf updates

--- html/protected/config/console.php
+++ html/protected/config/console.php
@@ -1,3 +1,30 @@
 <?php
 
-
+// console config
+return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => "WM Mobile - Console",
+    // preloading 'log' component
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.shared.models.dal.*', // data access logic classes
+        'application.shared.models.bll.*', // business  logic classes
+        'application.components.*',
+    ),
+    // application components
+    'components' => array(
+        'db' => array(
+            'connectionString' => "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME,
+            'emulatePrepare' => true,
+            'username' => DB_USER,
+            'password' => DB_PASS,
+            'charset' => 'UTF-8',
+            'schemaCachingDuration' => '3600',
+        ),
+        'authManager' => array(
+            'class' => 'CDbAuthManager',
+            'connectionID' => 'db',
+        ),
+    ),
+);
--- html/protected/yiic.php
+++ html/protected/yiic.php
@@ -2,9 +2,10 @@
 
 // change the following paths if necessary
 
+require_once(dirname(__FILE__).'/config/config.php');
+
 $yiic=dirname(__FILE__).'/../../framework/yiic.php';
 
-dirname(__FILE__).'/config/console.php';
 
 $config=dirname(__FILE__).'/config/console.php';
 
