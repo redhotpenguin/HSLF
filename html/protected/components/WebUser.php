4d1a06e6fff395681906ad7fd4145f9613f4a47a
Jonas Palmero <jonas@winningmark.com>
2013-01-22 16:14:10 -0800

    added caching

--- html/protected/components/WebUser.php
+++ html/protected/components/WebUser.php
@@ -2,6 +2,8 @@
 
 class WebUser extends CWebUser {
 
+    private $_access = array();
+
     public function __get($name) {
 
         if ($this->hasState('__userInfo')) {
@@ -40,15 +42,24 @@ class WebUser extends CWebUser {
     }
 
     /**
-     * override checkAccess
-     * Use current user ID (integer) instead of username
+     * Override 
+     * Performs access check for this user.
+     * @param string $operation the name of the operation that need access check.
+     * @param array $params name-value pairs that would be passed to business rules associated
+     * {@link getId()} when {@link CDbAuthManager} or {@link CPhpAuthManager} is used.
+     * @param boolean $allowCaching whether to allow caching the result of access check.
+     * @return boolean whether the operations can be performed by this user.
      */
     public function checkAccess($operation, $params = array(), $allowCaching = true) {
+        if ($allowCaching && isset($this->_access[$operation])) {
 
+            return $this->_access[$operation];
+        }
 
-        $id = Yii::app()->user->id;
-
-        $access = Yii::app()->getAuthManager()->checkAccess($operation, $id, $params);
+        $access = Yii::app()->getAuthManager()->checkAccess($operation, Yii::app()->user->id, $params);
+        if ($allowCaching) {            
+            $this->_access[$operation] = $access;
+        }
 
 
         return $access;
