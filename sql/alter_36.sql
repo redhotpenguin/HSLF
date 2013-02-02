4063c375fa8025102d34a75584ca143b684d5c09
Jonas Palmero <jonas@winningmark.com>
2013-02-02 10:06:08 -0800

    tag_organization alter file

--- sql/alter_36.sql
+++ sql/alter_36.sql
@@ -3,3 +3,9 @@ ALTER TABLE tag ADD COLUMN display_name TEXT;
 UPDATE tag SET display_name = name;
 
 ALTER TABLE tag ALTER COLUMN display_name SET NOT NULL;
+
+CREATE TABLE tag_organization(
+    tag_id INTEGER REFERENCES tag(id),
+    organization_id INTEGER REFERENCES organization(id),
+    PRIMARY KEY (tag_id, organization_id)
+);
