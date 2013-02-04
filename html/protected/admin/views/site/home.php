7d57b301bfe162167ade972813d737447db95b80
Jonas Palmero <jonas@winningmark.com>
2013-02-03 19:16:35 -0800

    boot strap upgrade

--- html/protected/admin/views/site/home.php
+++ html/protected/admin/views/site/home.php
@@ -12,24 +12,24 @@
           
               
             
-            <p> <b><?php echo $total_item_number; ?></b>  <?php  echo CHtml::link("Ballot Items", array("item/Admin")); ?> </p>
+            <b><?php echo $total_item_number; ?></b>   <?php  echo CHtml::link("Ballot Items", array("item/Admin")); ?> </p>
             <p> <b><?php echo $total_user_number; ?></b>  <?php  echo CHtml::link("Mobile Users", array("mobileUser/Index")); ?> </p>
 
         </div>
 
         <div class="row">
             <div class="span3">
-                <h2>Ballot Items</h2>
+                <h3>Ballot Items</h3>
                 <p>Manage ballot items. </p>
                 <p><?php  echo CHtml::link('More »', array('item/Admin'), array('class'=>'btn')); ?></a></p>
             </div>
             <div class="span3">
-                <h2>Share Payloads</h2>
+                <h3>Share Payloads</h3>
                 <p>Manage share payloads. </p>
                 <p><?php  echo CHtml::link('More »', array('sharePayload/index'), array('class'=>'btn')); ?></p>
             </div>
             <div class="span3">
-                <h2>Push Notifications</h2>
+                <h3>Push Notifications</h3>
                 <p>Send push notifications.</p>
                 <p><a class="btn" href="<?php echo $tenant->ua_dashboard_link; ?>">More »</a></p>
             </div>
@@ -37,19 +37,19 @@
         <br/>
         <div class="row">
             <div class="span3">
-                <h2>Organizations</h2>
+                <h3>Organizations</h3>
                 <p>Manage organizations.</p>
                 <p><?php  echo CHtml::link('More »', array('organization/admin'), array('class'=>'btn')); ?></p>
             </div>
 
             <div class="span3">
-                <h2>Options</h2>
+                <h3>Options</h3>
                 <p>Manage application's options.</p>
                 <p><?php  echo CHtml::link('More »', array('option/index'), array('class'=>'btn')); ?></p>
             </div>
 
             <div class="span3">
-                <h2>Mobile Users</h2>
+                <h3>Mobile Users</h3>
                 <p>Mobile users information.</p>
                 <p><?php  echo CHtml::link('More »', array('mobileUser/index'), array('class'=>'btn')); ?></p>
             </div>
