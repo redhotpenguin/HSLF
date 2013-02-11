0f06eadad0756828b2a0f5630843549c9a189273
Jonas Palmero <jonas@winningmark.com>
2013-02-11 09:40:31 -0800

    push messages tabs

--- html/protected/admin/views/pushMessage/editor.php
+++ html/protected/admin/views/pushMessage/editor.php
@@ -18,95 +18,45 @@ $this->widget('bootstrap.widgets.TbNavbar', array(
         ),
     ),
 ));
+
 ?>
+
 
 <?php
 $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     'id' => 'push-message-form',
     'enableAjaxValidation' => false,
         ));
-?>
-<div class="form-actions">
 
-    <p class="help-block">Fields with <span class="required">*</span> are required.</p>
+
+
+
+$pushMessageTab = $this->renderPartial('tabs/_tab_push_message', array(
+    'model' => $model,
+    'form' => $form,
+        ), true);
+
+$tagsTab = $this->renderPartial('tabs/_tab_tags', array(
+    'model' => $model,
+    'form' => $form,
+        ), true);
+
+$this->widget('bootstrap.widgets.TbTabs', array(
+    'type' => 'tabs', // 'tabs' or 'pills'
+    'placement' => 'left',
+    'tabs' => array(
+        array('label' => 'Push Message', 'content' => $pushMessageTab, 'active' => true),
+        array('label' => 'Tags', 'content' => $tagsTab),
+    ),
+));
+?>
+
 
     <?php echo $form->errorSummary($model); ?>
+    
 
-    <div>  
-
-        <?php echo $form->textAreaRow($model, 'alert', array('class' => 'span5', 'cols' => '150', 'rows' => '2')); ?>
-    </div>
-
-    <div>
-        <?php echo $form->labelEx($model, 'payload_id'); ?>
-        <?php
-        $payloadList = CHtml::listData(Payload::model()->findAll(), 'id', 'title');
-        $options = array(
-            'tabindex' => '0',
-            'empty' => '(not set)',
-        );
-        //  echo $form->dropDownList($model, 'payload_id', $payloadList);
-        ?>
-        <?php echo $form->error($model, 'payload_id'); ?>
-    </div>
-
-    <div>
-        <?php echo $form->hiddenField($model, 'payload_id'); ?>
-        <?php
-// ext is a shortcut for application.extensions
-        $this->widget('ext.ExtendedAutoComplete.ExtendedAutoComplete', array(
-            'name' => 'test_autocomplete',
-            'source' => $this->createUrl('payload/findPayload'),
-// attribute_value is a custom property that returns the 
-// name of our related object -ie return $model->related_model->name
-            'value' => $model->isNewRecord ? '' : $model->payload->title,
-            'options' => array(
-                'minLength' => 1,
-                'autoFill' => false,
-                'focus' => 'js:function( event, ui ) {
-            $( "#test_autocomplete" ).val( ui.item.title );
-            return false;
-        }',
-                'select' => 'js:function( event, ui ) {
-            $("#' . CHtml::activeId($model, 'payload_id') . '")
-            .val(ui.item.id);
-            return false;
-        }'
-            ),
-            'htmlOptions' => array('class' => 'input-1', 'autocomplete' => 'off'),
-            'methodChain' => '.data( "autocomplete" )._renderItem = function( ul, item ) {
-        return $( "<li></li>" )
-            .data( "item.autocomplete", item )
-            .append( "<a>" + item.title +  "</a>" )
-            .appendTo( ul );
-    };'
-        ));
-        
-        if(!$model->isNewRecord ){
-        //    echo $model->payload->title;
-        }
-        
-        ?>
-    </div>
-
-    <div>
-        <?php echo $form->textFieldRow($model, 'creation_date', array('class' => 'span2')); ?>
-    </div>
-
+    <div class="clearfix"></div>
     <hr/>
-
-    <h4>Tags:</h4>
-
-    <div class="row-fluid">
-        <?php
-        $this->widget('ext.TagSelector.TagSelector', array(
-            'model' => $model,
-            'tag_types' => array('alert', 'district')
-        ));
-        ?>
-    </div>
-
-
     <?php
     $this->widget('bootstrap.widgets.TbButton', array(
         'buttonType' => 'submit',
