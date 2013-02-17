2d5477364fe3e72c49738a912e84298f15a05081
Jonas Palmero <jonas@winningmark.com>
2013-02-16 17:26:41 -0800

    clean up and more obvious 404

--- html/protected/client/views/site/error404.php
+++ html/protected/client/views/site/error404.php
@@ -1,3 +1,11 @@
-<h1>404</h1>
-<h3>Page not found!</h3>
+<div class="not_found">
+    <p class="code">404</p>
+    <div class="clearfix"></div>
+    <p class="reason">This is not the page you are looking for.</p>
 
+
+
+</div>
+<?php
+if (Yii::app()->request->urlReferrer)
+    echo '<br/>' . CHtml::link('Go back', Yii::app()->request->urlReferrer, array('class' => 'btn'));
