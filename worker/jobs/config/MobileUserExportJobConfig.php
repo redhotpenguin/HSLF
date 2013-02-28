9ce36762a7c77e7e764176a8de564644c478fe1e
Jonas Palmero <jonas@winningmark.com>
2013-02-28 10:42:07 -0800

    language update

--- worker/jobs/config/MobileUserExportJobConfig.php
+++ worker/jobs/config/MobileUserExportJobConfig.php
@@ -40,7 +40,7 @@ $emailSubjectTemplate = "[Winning Mark Mobile] {name}, your user export is ready
 $emailBodyTemplate = "
 <div style='margin:20px;border-radius:10px; background-color:#efefef; padding:25px;color:#666;'><h1 style='color:#DD5336;'>Winning Mark Mobile</h1>    
 
-Hi <b>{name}</b>, <p> Your report is ready and avalaible at the following url: <a href='{downloadLink}'>{downloadLink}</a><p>If we can be of any assistance, please contact mobile@winningmark.com</p>
+Hi <b>{name}</b>, <p> Your user export is ready and avalaible at the following url: <a href='{downloadLink}'>{downloadLink}</a><p>If we can be of any assistance, please contact mobile@winningmark.com</p>
 </p><p>Thank you!</p><p style='margin-top: 30px;font-style:italic;color:#777;'>Export requested by {requester}</p>
 </div>";
 
