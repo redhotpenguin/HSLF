1eca171f0c59a73c8d7c02e3cabf3b39ae428fca
Jonas Palmero <jonas@winningmark.com>
2013-03-25 11:17:00 -0700

    tags: restrict export to admins

--- html/protected/client/controllers/TagController.php
+++ html/protected/client/controllers/TagController.php
@@ -19,7 +19,7 @@ class TagController extends Controller {
      */ public function accessRules() {
         return array(
             array('allow',
-                'actions' => array('index', 'exportCSV'),
+                'actions' => array('index'),
                 'roles' => array('readTag'),
             ),
             array('allow',
@@ -33,6 +33,11 @@ class TagController extends Controller {
             array('allow',
                 'actions' => array('delete'),
                 'roles' => array('deleteTag'),
+            ),
+            array(
+                'allow',
+                'actions' => array('exportCSV'),
+                'roles' => array('admin')
             ),
             array('deny', // deny all users
                 'users' => array('*'),
@@ -121,6 +126,7 @@ class TagController extends Controller {
 
         $this->render('index', array(
             'model' => $model,
+            'isAdmin' => Yii::app()->user->hasPermission('admin'),
         ));
     }
 
