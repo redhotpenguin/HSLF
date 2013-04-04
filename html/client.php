c8102f9c270fda0dc18ba1dd3ebbaa08d6f95dc5
Jonas Palmero <jonas@winningmark.com>
2013-04-04 15:19:46 -0700

    application level exception

--- html/client.php
+++ html/client.php
@@ -1,13 +1,20 @@
 <?php
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yiilite.php';
-require_once(dirname(__FILE__).'/protected/config/config.php');
-$config=dirname(__FILE__).'/protected/client/config/main.php';
 
-require ( 'protected/helpers/globals.php' ) ;
+// change the following paths if necessary
+$yii = dirname(__FILE__) . '/../framework/yiilite.php';
+require_once(dirname(__FILE__) . '/protected/config/config.php');
+$config = dirname(__FILE__) . '/protected/client/config/main.php';
+
+require ( 'protected/helpers/globals.php' );
 
 require_once($yii);
+try {
+    $yii = Yii::createWebApplication($config);
 
-$yii =Yii::createWebApplication($config);
-
-$yii->run();
+    $yii->run();
+} catch (Exception $e) {
+    header('HTTP/1.1 503 Service Temporarily Unavailable');
+    header('Status: 503 Service Temporarily Unavailable');
+    header('Retry-After: 60');
+    echo '<h1>Service unavailable</h1>';
+}
