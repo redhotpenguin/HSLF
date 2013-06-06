2e2fa9eb0449d860e0498ff125bf6419b6d0170a
Jonas Palmero <jonas@winningmark.com>
2013-06-06 10:28:08 -0700

    updated district resolver api test

--- html/protected/tests/unit/APITest.php
+++ html/protected/tests/unit/APITest.php
@@ -33,10 +33,14 @@ class APITest extends CDbTestCase {
         $this->assertNotEmpty($result->results);
     }
 
-    public function testGetDistrictsByAddress() {
+    public function testDistrictResolverAPI() {
         $result = $this->getResource('DistrictResolver/?address=4261%20glacier%20lily%20st%20lake%20oswego%20oregon&districts=legislative,nonlegislative/census', true);
         $this->assertEquals(200, $result->status);
         $this->assertNotEmpty($result->results);
+        
+        $result2 = $this->getResource('DistrictResolver/?lat=45.548294&long=-122.725525&districts=legislative,nonlegislative/census', true);
+        $this->assertEquals(200, $result2->status);
+        $this->assertNotEmpty($result2->results);
     }
 
     public function testItemNewsAPI() {
