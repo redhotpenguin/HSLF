a17eccac1e2afa8822005949f2bb158cc6c6ae25
Jonas Palmero <jonas@winningmark.com>
2013-06-07 09:33:32 -0700

    login box tweaks

--- html/protected/client/views/site/login.php
+++ html/protected/client/views/site/login.php
@@ -4,9 +4,10 @@ $this->pageTitle = Yii::app()->name . ' - Login';
 
 
 <div class="row-fluid">
-    <div class="login-box">
-        <h2>Login to your account</h2>
 
+
+    <div class="login-box">
+        <h3>Log In</h3>
         <?php
         $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
             'id' => 'login-form',
@@ -18,44 +19,36 @@ $this->pageTitle = Yii::app()->name . ' - Login';
             ),
                 ));
         ?>
-        <fieldset>
 
-            <div class="input-prepend" title="Username">
-                <span class="add-on"><i class="halflings-icon user"></i></span>
+        <div class="input-prepend" title="Username">
+            <span class="add-on"><i class="halflings-icon user"></i></span>
 
-                <?php
-                echo $form->textField($model, 'username', array('class' => 'input-large span10', 'placeholder' => 'username'));
-                ?>
-
-
-            </div>
+            <?php
+            echo $form->textField($model, 'username', array('class' => 'input-large span10', 'placeholder' => 'username'));
+            ?>
+        </div>
 
 
-            <div class="clearfix"></div>
+        <div class="input-prepend" title="Password">
+            <span class="add-on"><i class="halflings-icon lock"></i></span>
+            <?php
+            echo $form->passwordField($model, 'password', array('class' => 'input-large span10', 'placeholder' => 'password'));
+            ?>
+        </div>
 
-            <div class="input-prepend" title="Password">
-                <span class="add-on"><i class="halflings-icon lock"></i></span>
-                <?php
-                echo $form->passwordField($model, 'password', array('class' => 'input-large span10', 'placeholder' => 'password'));
-                ?>
-            </div>
+        <div class="prepend">
+            <?php
+            echo $form->error($model, 'username');
 
-            <div class="clearfix"></div>
-            
-            <div class="prepend">
-                <?php
-                echo $form->error($model, 'username');
+            echo $form->error($model, 'password');
+            ?>
+        </div>
 
-                echo $form->error($model, 'password');
-                ?>
-            </div>
+        <div class="prepend button-login">	
+            <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType' => 'submit', 'type' => 'primary', 'label' => 'Login'));
+            ?>
+        </div>
 
-            <div class="button-login">	
-                <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType' => 'submit', 'type' => 'primary', 'label' => 'Login'));
-                ?>
-            </div>
-
-        </fieldset>
         <?php $this->endWidget(); ?>
     </div>
 </div>
