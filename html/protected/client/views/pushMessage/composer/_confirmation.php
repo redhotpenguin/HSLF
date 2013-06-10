e8671c6331a4a0a74ad89d59e9ae62418eb0b5c5
Jonas Palmero <jonas@winningmark.com>
2013-06-10 13:30:36 -0700

    tweaks

--- html/protected/client/views/pushMessage/composer/_confirmation.php
+++ html/protected/client/views/pushMessage/composer/_confirmation.php
@@ -1,19 +1,13 @@
-<fieldset id="thankyou">
+<div class="hero-unit">
+    <h1>Thank you</h1>
+    <p>"<?php echo $pushMessage->alert; ?>" was successfully saved.</p>
+</div>
 
+<?php
+echo "<p><em>Push Message ID: #{$pushMessage->id}</em><br/>";
+if ($pushMessage->payload->type != 'other') {
+    echo "<em>Payload ID: #{$pushMessage->payload->id}</em>";
+}
 
-    <div class="hero-unit">
-        <h1>Thank you</h1>
-        <p>"<?php echo $pushMessage->alert; ?>" was successfully saved.</p>
-    </div>
-
-    <?php
-    echo "<p><em>Push Message ID: #{$pushMessage->id}</em><br";
-    if ($pushMessage->payload->type != 'other') {
-        echo "<em>Payload ID: #{$pushMessage->payload->id}</em>";
-    }
-
-        echo '</p><p>'.CHtml::link('Go back to push messages', array('pushMessage/index')).'</p>';
-    ?>
-
-
-</fieldset>
+echo '</p><p>' . CHtml::link('Go back to push messages', array('pushMessage/index')) . '</p>';
+?>
