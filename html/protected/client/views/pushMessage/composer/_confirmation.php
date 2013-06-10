a685987938e96432b1206af0f1e4679d31f6a4ff
Jonas Palmero <jonas@winningmark.com>
2013-06-10 12:53:25 -0700

    confirmation screen tweaks

--- html/protected/client/views/pushMessage/composer/_confirmation.php
+++ html/protected/client/views/pushMessage/composer/_confirmation.php
@@ -1,6 +1,15 @@
 <fieldset id="thankyou">
-    <h1>Thank you</h1>
-    
-    <p>"<?php echo $pushMessage->alert; ?>" was successfully sent</p>
+
+
+    <div class="hero-unit">
+        <h1>Thank you</h1>
+        <p>"<?php echo $pushMessage->alert; ?>" was successfully saved.</p>
+    </div>
+
+    <?php
+    if ($pushMessage->payload->type != 'other') {
+        echo "<em>Payload ID: #{$pushMessage->payload->id}</em>";
+    }
+    ?>
 
 </fieldset>
