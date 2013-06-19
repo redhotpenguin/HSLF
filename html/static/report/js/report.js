5c6256b5ab8d262a053773e43c6a225d4ac6b27b
Jonas Palmero <jonas@winningmark.com>
2013-06-19 11:07:47 -0700

    skip consecutive zeros and graph animation

--- html/static/report/js/report.js
+++ html/static/report/js/report.js
@@ -2,70 +2,85 @@ jQuery(document).ready(report);
 
 
 function report($){
-    var s1 = [];
-    var ticks = [];
+    function displayMonthlyPushReport(){
+        var s1 = [];
+        var ticks = [];
     
-    $.get(report_ns.controller_url + '/report/monthlyJsonReport', function(report){
-        var l  = report['sends'].length;
-        
-        for(var i = 0; i < l; i++){
-            var r = report['sends'][i],
-            total = r.android + r.ios;
+        $.get(report_ns.controller_url + '/report/monthlyJsonReport', function(report){
+            var l  = report['sends'].length;
+            var prevTotal = 1;
+            for(var i = 0; i < l; i++){
+                var r = report['sends'][i],
+                total = r.android + r.ios;
+            
+                if(total == 0 &&  prevTotal  == 0  ){ // skip consecutive 0s
+                    continue;
+                }    
             
             
-            var d=new Date(Date.parse(r.date)),
-            month = d.getMonth() + 1; // month sare zero based (jan = 0)
+                var d=new Date(Date.parse(r.date)),
+                month = d.getMonth() + 1; // month sare zero based (jan = 0)
             
-            s1.push(total);
-            ticks.push( month + "/" + d.getDate() );
-        }
-
-     
-        var plot1 = $.jqplot('monthlyPushChart', [s1], {
-            axesDefaults:{
-                min:0
-            },
-            seriesDefaults:{
-                renderer:$.jqplot.BarRenderer,
-                rendererOptions: {
-                    fillToZero: false
-                }
-            },
-
-            legend: {
-                show: false
-            },
-            axes: {
-                xaxis: {
-                    renderer: $.jqplot.CategoryAxisRenderer,
-                    ticks: ticks,
-                    tickOptions:{
-                        showGridline: false
-                    }
-                },
-                yaxis: {
-                    pad: 1.05,
-                    tickOptions: {
-                        formatString: '%d',
-                        showGridline: true
-                    }
-                }
-            },
-            highlighter: {
-                show: true,
-                sizeAdjust: 7.5,
-                formatString: "%d",
-                tooltipContentEditor: function(str, seriesIndex, pointIndex, jqPlot) {
-                    return s1[pointIndex];
-                }
-
-            }, 
-            cursor: {
-                show: false
+                s1.push(total);
+                ticks.push( month + "/" + d.getDate() );
+            
+                prevTotal =  total;
             }
 
+     
+            $.jqplot('monthlyPushChart', [s1], {
+                animate: true,
+                axesDefaults:{
+                    min:0
+                },
+                seriesDefaults:{
+                    renderer:$.jqplot.BarRenderer,
+                    rendererOptions: {
+                        fillToZero: false,
+                        animation: {
+                            speed: 1000
+                        }
+                    }
+                },
+
+                legend: {
+                    show: false
+                },
+                axes: {
+                    xaxis: {
+                        renderer: $.jqplot.CategoryAxisRenderer,
+                        ticks: ticks,
+                        tickOptions:{
+                            showGridline: false
+                        }
+                    },
+                    yaxis: {
+                        pad: 1.05,
+                        tickOptions: {
+                            formatString: '%d',
+                            showGridline: true
+                        }
+                    }
+                },
+                highlighter: {
+                    show: true,
+                    sizeAdjust: 7.5,
+                    formatString: "%d",
+                    tooltipContentEditor: function(str, seriesIndex, pointIndex, jqPlot) {
+                        return s1[pointIndex];
+                    }
+
+                }, 
+                cursor: {
+                    show: false
+                }
+
+            });
+    
+    
         });
     
+    }  // displayMonthlyPushReport end
     
-    });
+    displayMonthlyPushReport();
 } // jquery ready/end
