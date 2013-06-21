0de139b5af342d9f9f1146df6601beed919954db
Jonas Palmero <jonas@winningmark.com>
2013-06-21 13:26:58 -0700

    ready for merge

--- html/protected/client/extensions/TagSelector/assets/tag_creator.js
+++ html/protected/client/extensions/TagSelector/assets/tag_creator.js
@@ -1,16 +1,67 @@
 jQuery(document).ready(tagCreator);
 
 function tagCreator($){
-    /*
-    var tagNameInput = $('#new_tag_form [name="name"]')
-    
-    $('#new_tag_form [name="display_name"]').keyup(function(){
-        var value = $(this).val();
-        tagNameInput .val(value.toLowerCase().replace(/ /g,"_").replace(/\W/g, ''));
-    });
-    
-    $("#save_tag_btn").click(function(){
-                 
+   
+
+
+
+
+  
+    var modelTagTable = $("#modelTagTable"),
+    tagNameInput = $('#new_tag_form [name="name"]'),
+    addItem = function(name, id){
+        var row = '<tr name="tagRow"><td> '+ name + '</td>';
+        row += '<input value="'+ id +'" type="hidden" name='+ tagSelector_ns.modelName +'[tags][]">';
+        row += '<td><span name="deleteTagBtn" class=" btn btn-warning" >remove</span></td>';    
+        row +='</tr>';
+        
+        
+        if(  modelTagTable.find('tr[name="tagRow"]').length == 0){
+            modelTagTable.find('tbody').after(row);
+        }else{
+            modelTagTable.find('tr[name="tagRow"]:last').after(row);
+        }
+
+    },
+    tagTableUpdated = function(e){
+        console.log('cl');
+        $(this).parents("tr[name='tagRow']").first().remove();
+    },
+    autoCompleteInit = function(){
+        var encodedTypeString = "";
+        tagSelector_ns.tagTypes.map(function(type){
+            encodedTypeString += "&types[]="+type;
+        });
+            
+        $( "#searchTag" ).autocomplete({
+            minLength: 2,
+            delay: 50, 
+            source: function(request, response){
+           
+                response(['Loading..']);
+           
+                var tagFinderUrl = tagSelector_ns.site_url + '/tag/findTag?term=' + request.term + encodedTypeString;
+            
+                $.get(tagFinderUrl, function(tags){
+                    response( tags.map(function(tag){
+                        return {
+                            label: tag.display_name,
+                            value: tag.id
+                        }
+                    }));
+                
+                });
+            }
+
+        }).on("autocompleteselect", function(event, ui){
+            addItem( ui.item.label, ui.item.value );
+            $(this).val('');
+            return false;
+        
+        });
+    }
+    saveTag = function(){
+                       
         var tagType = $('[name="type"]').val(),
         tagName = $('[name="name"]').val(),
         tagDisplayName = $('[name="display_name"]').val();
@@ -31,79 +82,24 @@ function tagCreator($){
             $('[name="display_name"]').val('');
             $('[name="name"]').val('');
             if(tag.id != undefined)
-                updateTagTable(tag);
+                addItem(tag.display_name, tag.id);
         });
-    });
-    
-    function updateTagTable(tag){
-        
-        var row = '<tr><td>'+tag.display_name+'</td>';
-        
-        row += '<td><input value="'+tag.id+'" type="checkbox" name='+tagSelector_ns.modelName+'[tags][]"></td>';
-        
-        row += '</tr>';
-   
-        $('#table_tag tr:last').after(row);
-        
-    }
-   */
-  
-    var modelTagTable = $("#modelTagTable"),
-    addItem = function(name, id){
-        var row = '<tr name="tagRow"><td> '+ name + '</td>';
-        row += '<input value="'+ id +'" type="hidden" name='+ tagSelector_ns.modelName +'[tags][]">';
-        row += '<td><span name="deleteTagBtn" class=" btn btn-warning" >remove</span></td>';    
-        row +='</tr>';
-        
-        
-        if(  modelTagTable.find('tr[name="tagRow"]').length == 0){
-            modelTagTable.find('tbody').after(row);
-        }else{
-            modelTagTable.find('tr[name="tagRow"]:last').after(row);
-        }
-
     },
-    tagTableUpdated = function(e){
-        console.log('cl');
-        $(this).parents("tr[name='tagRow']").first().remove();
+    createTagName = function(){
+        var value = $(this).val();
+        tagNameInput .val(value.toLowerCase().replace(/ /g,"_").replace(/\W/g, ''));
     };
     
 
-    var encodedTypeString = "";
-    tagSelector_ns.tagTypes.map(function(type){
-        encodedTypeString += "&types[]="+type;
-    });
-            
-    $( "#searchTag" ).autocomplete({
-        minLength: 2,
-        delay: 50, 
-        source: function(request, response){
-           
-           response(['Loading...']);
-           
-            var tagFinderUrl = tagSelector_ns.site_url + '/tag/findTag?term=' + request.term + encodedTypeString;
-            
-            $.get(tagFinderUrl, function(tags){
-                response( tags.map(function(tag){
-                    return {
-                        label: tag.display_name,
-                        value: tag.id
-                    }
-                }));
-                
-            });
-        }
-
-    }).on("autocompleteselect", function(event, ui){
-        addItem( ui.item.label, ui.item.value );
-        $(this).val('');
-        return false;
-        
-    });
+   
     
     // event binding
-    // modelTagTable.find("span[name='deleteTagBtn']").bind('click', tagTableUpdated);
     modelTagTable.on("click", "span[name='deleteTagBtn']", tagTableUpdated);
+    $('#new_tag_form [name="display_name"]').keyup(createTagName);
+    $("#save_tag_btn").click(saveTag);
+
+    // init
+    autoCompleteInit();
    
    
 }
