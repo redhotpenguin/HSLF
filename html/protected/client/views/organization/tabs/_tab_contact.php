aac350a0fcddf2497c281112427e69b0e243601e
Jonas Palmero <jonas@winningmark.com>
2013-07-30 06:39:58 -0700

    language

--- html/protected/client/views/organization/tabs/_tab_contact.php
+++ html/protected/client/views/organization/tabs/_tab_contact.php
@@ -5,7 +5,9 @@
         'attribute' => '',
         'options' => array(
             'model_name' => 'Organization',
-            'help_text' => 'place holder',
+            'help_text' => 'Assign existing contacts to an Organization from the drop down menu. Remove a contact from an Organization by clicking “Remove.” Create a new contact and assign it to an Organization by clicking “Create New Contact.”
+Edit the order contacts appear in on the Organization Detail Page by clicking and dragging the name to the position you want.
+Click “Save” when you are done making changes.',
             'dropdown_text' => 'Select contact to assign to organization'
         ),
     ));
