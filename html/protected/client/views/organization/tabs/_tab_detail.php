2f35a4c22507c27dff765bde54f778c2cd1a27ae
Jonas Palmero <jonas@winningmark.com>
2013-07-29 18:15:50 -0700

    language tweaks - map-750

--- html/protected/client/views/organization/tabs/_tab_detail.php
+++ html/protected/client/views/organization/tabs/_tab_detail.php
@@ -3,7 +3,7 @@
     <?php
     echo $form->labelEx($model, 'description');
     ?>
-    <a href="#" class="icon-question-sign" rel="tooltip" data-placement="right" title="Add text, links to contracts and other information to the Organization Detail Page. To add a new link, place the cursor in the editor and click “Link.” To edit an existing link, highlight the link in the editor and click “Link” to update the URL."></a>
+    <a href="#" class="icon-question-sign" rel="tooltip" data-placement="right" title="Add text, links and other information to the Organization Detail Page. To add a new link, place the cursor in the editor and click “Link.” To edit an existing link, highlight the link in the editor and click “Link” to update the URL."></a>
     <div class="clearfix"></div>
     <?php
     $this->widget('backend.extensions.tinymce.TinyMce', array(
