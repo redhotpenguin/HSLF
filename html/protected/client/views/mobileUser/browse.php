24c51555203402ffa507566b228f459df8a4b0b3
Jonas Palmero <jonas@winningmark.com>
2013-08-02 19:04:30 -0700

    map788

--- html/protected/client/views/mobileUser/browse.php
+++ html/protected/client/views/mobileUser/browse.php
@@ -2,6 +2,10 @@
 $this->secondaryNav['name'] = 'Mobile Users';
 $this->secondaryNav['url'] = array('mobileUser/index');
 
+$this->introText = 'Below is a list of all users registered with your app. To search by ID, Name or Email, type in a search bar and press enter. To filter by Device Type, select from the drop down menu.</p><p class="helpText">Click the "View" icon to see details about the user or the "Delete" icon to remove the user from the dashboard; this will not remove the app from the user’s phone but will delete the user record from your database. If the user opens the app again, a new record will be created.';
+
+
+
 Yii::app()->clientScript->registerScript('search', "
     
 function submitSearchForm(){
