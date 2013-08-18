bfab10c79f4119952c8636718dbd19861b93fa35
Jonas Palmero <jonas@winningmark.com>
2013-08-18 08:26:13 -0700

    404 improvements

--- html/protected/client/views/site/error404.php
+++ html/protected/client/views/site/error404.php
@@ -1,20 +1,12 @@
 <div class="not_found">
     <p class="code">404</p>
     <div class="clearfix"></div>
-    <p class="reason">
-        <?php
-        if (isset($error['message'])):
-            echo $error['message'];
-        else :
-            ?>
-        <p>  This is not the page you are looking for. <br/>
-            These may be the droids you are looking for.
-        </p>
-        <iframe width="620" height="340" src="https://www.youtube.com/embed/g5VR4wdGeRg?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
-    <?php
-    endif;
-    ?>
-</p>
+    <p class="reason">This is not the page you are looking for. <br/>
+These may be the droids you are looking for.
+    </p>
+
+    <iframe width="620" height="340" src="https://www.youtube.com/embed/g5VR4wdGeRg?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
+
 
 
 
