b5346ce3b17e67e45eae72a0886502588ad6b783
Jonas Palmero <jonas@winningmark.com>
2013-08-22 20:19:48 -0700

    map 871

--- html/protected/client/views/site/index.php
+++ html/protected/client/views/site/index.php
@@ -1,45 +1,46 @@
 <div class="hero-unit">
     <h1>Dashboard</h1>
 </div>
-<div class="tenantGroup action_group">
-    <?php
+<?php
 
-    // UI Helper
-    function getBlockClassSize($number) {
-        $classBlock = 'whole';
+// UI Helper
+function getBlockClassSize($number) {
+    $classBlock = 'whole';
 
-        if ($number == 4) {
-            $classBlock = 'fourth';
-        } elseif ($number == 3) {
-            $classBlock = 'third';
-        } elseif ($number == 2) {
-            $classBlock = 'half';
-        }
-
-        return $classBlock;
+    if ($number == 4) {
+        $classBlock = 'fourth';
+    } elseif ($number == 3) {
+        $classBlock = 'third';
+    } elseif ($number == 2) {
+        $classBlock = 'half';
     }
 
-    usort($tenants, function($a, $b) {
-                return strcmp($a->display_name, $b->display_name);
-            });
+    return $classBlock;
+}
 
-    $tenantNumber = count($tenants);
-    ?>
+usort($tenants, function($a, $b) {
+            return strcmp($a->display_name, $b->display_name);
+        });
 
-    <div class="section-divider">
-        <h3>My Project<?php echo $tenantNumber > 1 ? 's' : ''; ?></h3>
-    </div>
+$tenantNumber = count($tenants);
+?>
+
+<div class="section-divider">
+    <h3>My Project<?php echo $tenantNumber > 1 ? 's' : ''; ?></h3>
+</div>
+
+<div class="tenantGroup action_group">
+
 
     <?php
-
     $tenantGroups = array_chunk($tenants, 4);
 
     foreach ($tenantGroups as $tenants) {
-        
+
         $blockClass = getBlockClassSize(count($tenants));
 
         foreach ($tenants as $tenant) {
-             echo CHtml::link($tenant->display_name, "/client/" . $tenant->name, array('class' => "action_block $blockClass"));
+            echo CHtml::link($tenant->display_name, "/client/" . $tenant->name, array('class' => "action_block $blockClass"));
         }
     }
     ?>
